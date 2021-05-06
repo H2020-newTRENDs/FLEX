52440ee82038b373a043ca56470aa4ec964ce594
Yu <songmin.yu@isi26.isi.fhg.de>
2021-05-06 17:22:16 +0200

    optimization update

--- C_Model/C2_OperationOptimization/C21_OperationOptimization.py
+++ C_Model/C2_OperationOptimization/C21_OperationOptimization.py
@@ -3,7 +3,7 @@ from A_Infrastructure.A1_Config.A12_Register import REG
 from A_Infrastructure.A2_ToolKits.A21_DB import DB
 from B_Classes.B1_Household import Household
 
-class OptimizationCore:
+class OperationOptimization:
 
     """
     Intro
@@ -19,15 +19,29 @@ class OptimizationCore:
     def __init__(self, conn):
         self.Conn = conn
         self.ID_Household = DB().read_DataFrame(REG().Gen_OBJ_ID_Household, self.Conn)
-        # self.ID_Environment = DB().read_DataFrame(REG().Gen_Sec_ID_Environment, self.Conn)
+        self.ID_Environment = DB().read_DataFrame(REG().Gen_Sce_ID_Environment, self.Conn)
+        self.Radiation = DB().read_DataFrame(REG().Sce_Weather_Radiation, self.Conn)
+        self.Temperature = DB().read_DataFrame(REG().Sce_Weather_Temperature, self.Conn)
+        self.HeatPumpCOP = DB().read_DataFrame(REG().Sce_Technology_HeatPumpCOP, self.Conn)
+        # you can import all the necessary tables into the memory here.
+        # Then, it can be faster when we run the optimization problem for many "household - enviornment" combinations.
 
-    def gen_Household(self, para_series):
-        HouseholdAgent = Household(para_series, self.Conn)
-        print(HouseholdAgent.SpaceHeating.TankSize)
+    def gen_Household(self, row_id):
+        ParaSeries = self.ID_Household.iloc[row_id]
+        HouseholdAgent = Household(ParaSeries, self.Conn)
+        return HouseholdAgent
+
+    def gen_Environment(self, row_id):
+        EnvironmentSeries = self.ID_Environment.iloc[row_id]
+        return EnvironmentSeries
+
+    def run_Optimization(self, household_id, environment_id):
+        Household = self.gen_Household(household_id)
+        Environment = self.gen_Environment(environment_id)
         pass
 
     def run(self):
-        self.gen_Household(self.ID_Household.iloc[0])
+        self.run_Optimization(0, 0)
         pass
 
 
