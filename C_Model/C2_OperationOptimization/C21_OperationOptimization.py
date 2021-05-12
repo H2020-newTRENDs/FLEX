ad63e411635711cbe73fce184c56f349d14374f6
Yu <songmin.yu@isi26.isi.fhg.de>
2021-05-12 12:37:53 +0200

    minor revision

--- C_Model/C2_OperationOptimization/C21_OperationOptimization.py
+++ C_Model/C2_OperationOptimization/C21_OperationOptimization.py
@@ -20,9 +20,9 @@ class OperationOptimization:
         self.Conn = conn
         self.ID_Household = DB().read_DataFrame(REG().Gen_OBJ_ID_Household, self.Conn)
         self.ID_Environment = DB().read_DataFrame(REG().Gen_Sce_ID_Environment, self.Conn)
-        self.Radiation = DB().read_DataFrame(REG().Sce_Weather_Radiation, self.Conn)
-        self.Temperature = DB().read_DataFrame(REG().Sce_Weather_Temperature, self.Conn)
-        self.HeatPumpCOP = DB().read_DataFrame(REG().Sce_Technology_HeatPumpCOP, self.Conn)
+        # self.Radiation = DB().read_DataFrame(REG().Sce_Weather_Radiation, self.Conn)
+        # self.Temperature = DB().read_DataFrame(REG().Sce_Weather_Temperature, self.Conn)
+        # self.HeatPumpCOP = DB().read_DataFrame(REG().Sce_Technology_HeatPumpCOP, self.Conn)
         # you can import all the necessary tables into the memory here.
         # Then, it can be faster when we run the optimization problem for many "household - enviornment" combinations.
 
@@ -35,13 +35,24 @@ class OperationOptimization:
         EnvironmentSeries = self.ID_Environment.iloc[row_id]
         return EnvironmentSeries
 
+    def stateTransition_SpaceHeatingTankTemperature(self, Household,
+                                                    energy_from_boiler_to_tank,
+                                                    energy_from_tank_to_room):
+        TankTemperature = Household.SpaceHeating.calc_TankTemperature(energy_from_boiler_to_tank,
+                                                                      energy_from_tank_to_room)
+
+        return TankTemperature
+    
     def run_Optimization(self, household_id, environment_id):
         Household = self.gen_Household(household_id)
-        Environment = self.gen_Environment(environment_id)
+        # Environment = self.gen_Environment(environment_id)
+
         pass
 
     def run(self):
-        self.run_Optimization(0, 0)
+        for household_id in range(0, 100):
+            for environment_id in range(0, 100):
+                self.run_Optimization(household_id, environment_id)
         pass
 
 
