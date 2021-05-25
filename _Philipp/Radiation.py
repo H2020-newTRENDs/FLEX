81638a90786b429f5b9e1a79d48802e092cc454b
PhilippMaschi <phil.masch@hotmail.com>
2021-05-25 09:48:30 +0200

    Philipp: radiation ohne division von sin ergibt realistische ergebnisse

--- _Philipp/Radiation.py
+++ _Philipp/Radiation.py
@@ -1,21 +1,28 @@
 import numpy as np
 import datetime
+
+import pysolar.radiation
 import pysolar.solar as pysol
+from pysolar import radiation
 import pandas as pd
 from A_Infrastructure.A2_ToolKits.A21_DB import DB
 from A_Infrastructure.A1_Config.A12_Register import REG
 from A_Infrastructure.A1_Config.A11_Constants import CONS
 
-def calculate_angels_of_sun(latitude, longitude, timearray, E_dir_horizontal):
+def calculate_angels_of_sun(latitude, longitude, timearray):
     #  Der Azimutwinkel stellt den Horizontalwinkel der Sonne dar und beschreibt ihre Position in horizontaler Richtung
     #  durch die Abweichung von der Himmelsrichtung Süd.
     # Der Höhenwinkel (altitude) beschreibt die Höhedes Sonnenstandes und wird von der Horizontalebene aus in
     # Richtung Zenit gemessen.
     altitude_sun = np.array([])
     azimuth_sun = np.array([])
-    for i, time in enumerate(timearray):
-        altitude_sun = np.append(altitude_sun, pysol.get_altitude(latitude, longitude, time.to_pydatetime()))
+    E_dir_horizontal = np.array([])
+    for time in timearray:
+        altitude_sun_1 = pysol.get_altitude(latitude, longitude, time.to_pydatetime())
+        altitude_sun = np.append(altitude_sun, altitude_sun_1)
         azimuth_sun = np.append(azimuth_sun, pysol.get_azimuth(latitude, longitude, time.to_pydatetime()))
+        E_dir_horizontal = np.append(E_dir_horizontal,
+                                     radiation.get_radiation_direct(time.to_pydatetime(), altitude_sun_1))
 
     # azimuth_sun = azimuth_sun - 180  # weil pysolar bei nord=0 anfängt und nicht süd=0
 
@@ -32,28 +39,29 @@ def calculate_angels_of_sun(latitude, longitude, timearray, E_dir_horizontal):
     # west:
     azimuth_west = 270
 
-    # Einstrahlungswinkel Theta
-    theta_ost = np.arccos(-np.cos(np.deg2rad(altitude_sun)) * np.sin(np.deg2rad(gamma_fenster)) *
-                          np.cos(np.deg2rad(azimuth_sun - azimuth_ost - 180)) +
-                          np.sin(np.deg2rad(altitude_sun)) * np.cos(np.deg2rad(gamma_fenster)))
+    # Einstrahlungswinkel Theta https://link.springer.com/content/pdf/10.1007%2F978-3-8348-8237-0_2.pdf
+    cos_theta_ost = np.cos(np.deg2rad(altitude_sun)) * np.cos(np.deg2rad(gamma_fenster)) + \
+                    np.cos(np.deg2rad(altitude_sun)) * np.sin(np.deg2rad(gamma_fenster)) * \
+                    np.cos(np.deg2rad(azimuth_sun - azimuth_ost))
 
-    theta_west = np.arccos(-np.cos(np.deg2rad(altitude_sun)) * np.sin(np.deg2rad(gamma_fenster)) *
-                           np.cos(np.deg2rad(azimuth_sun - azimuth_west - 180)) +
-                           np.sin(np.deg2rad(altitude_sun)) * np.cos(np.deg2rad(gamma_fenster)))
+    cos_theta_west = np.cos(np.deg2rad(altitude_sun)) * np.cos(np.deg2rad(gamma_fenster)) + \
+                     np.cos(np.deg2rad(altitude_sun)) * np.sin(np.deg2rad(gamma_fenster)) * \
+                     np.cos(np.deg2rad(azimuth_sun - azimuth_west))
 
-    theta_sued = np.arccos(-np.cos(np.deg2rad(altitude_sun)) * np.sin(np.deg2rad(gamma_fenster)) *
-                           np.cos(np.deg2rad(azimuth_sun - azimuth_sued - 180)) +
-                           np.sin(np.deg2rad(altitude_sun)) * np.cos(np.deg2rad(gamma_fenster)))
+    cos_theta_sued = np.cos(np.deg2rad(altitude_sun)) * np.cos(np.deg2rad(gamma_fenster)) + \
+                     np.cos(np.deg2rad(altitude_sun)) * np.sin(np.deg2rad(gamma_fenster)) * \
+                     np.cos(np.deg2rad(azimuth_sun - azimuth_sued))
 
-    theta_nord = np.arccos(-np.cos(np.deg2rad(altitude_sun)) * np.sin(np.deg2rad(gamma_fenster)) *
-                           np.cos(np.deg2rad(azimuth_sun - azimuth_nord - 180)) +
-                           np.sin(np.deg2rad(altitude_sun)) * np.cos(np.deg2rad(gamma_fenster)))
+    cos_theta_nord = np.cos(np.deg2rad(altitude_sun)) * np.cos(np.deg2rad(gamma_fenster)) + \
+                     np.cos(np.deg2rad(altitude_sun)) * np.sin(np.deg2rad(gamma_fenster)) * \
+                     np.cos(np.deg2rad(azimuth_sun - azimuth_nord))
+
 
     # Direkt radiation:
-    E_dir_sued = E_dir_horizontal * np.cos(theta_sued) / np.sin(np.deg2rad(altitude_sun))
-    E_dir_west = E_dir_horizontal * np.cos(theta_west) / np.sin(np.deg2rad(altitude_sun))
-    E_dir_ost = E_dir_horizontal * np.cos(theta_ost) / np.sin(np.deg2rad(altitude_sun))
-    E_dir_nord = E_dir_horizontal * np.cos(theta_nord) / np.sin(np.deg2rad(altitude_sun))
+    E_dir_sued = E_dir_horizontal * cos_theta_sued #/ np.sin(np.deg2rad(altitude_sun))
+    E_dir_west = E_dir_horizontal * cos_theta_west #/ np.sin(np.deg2rad(altitude_sun))
+    E_dir_ost = E_dir_horizontal * cos_theta_ost #/ np.sin(np.deg2rad(altitude_sun))
+    E_dir_nord = E_dir_horizontal * cos_theta_nord #/ np.sin(np.deg2rad(altitude_sun))
 
     # diffuse Strahlung:
     E_diff_ost = E_dir_horizontal * (1 + np.cos(np.deg2rad(azimuth_ost))) / 2
@@ -72,7 +80,7 @@ def calculate_angels_of_sun(latitude, longitude, timearray, E_dir_horizontal):
     E_ost = E_ost.clip(min=0)
     E_nord = E_nord.clip(min=0)
 
-    return azimuth_sun, altitude_sun, E_nord, E_sued, E_ost, E_west
+    return azimuth_sun, altitude_sun, E_nord, E_sued, E_ost, E_west, E_dir_horizontal
 
 
 
@@ -83,13 +91,13 @@ if __name__ == "__main__":
     # testing the skript
     latitude = 48.193461
     longitude = 16.352117
-    timearray = pd.date_range("01-01-2010", "01-01-2011", freq="H", closed="left",
+    timearray = pd.date_range("01-01-2010 00:00:00", "01-01-2011 00:00:00", freq="H", closed="left",
                               tz=datetime.timezone.utc)
 
-    data = DB().read_DataFrame(REG().Sce_Weather_Radiation, conn=DB().create_Connection(CONS().RootDB), ID_Country=20)
-    E_dir = data.loc[:, "Radiation"].to_numpy()
+    # data = DB().read_DataFrame(REG().Sce_Weather_Radiation, conn=DB().create_Connection(CONS().RootDB), ID_Country=20)
+    # E_dir = data.loc[:, "Radiation"].to_numpy()
 
-    azimuth_sun, altitude_sun, E_nord, E_sued, E_ost, E_west = calculate_angels_of_sun(latitude, longitude, timearray, E_dir)
+    azimuth_sun, altitude_sun, E_nord, E_sued, E_ost, E_west, E_dir = calculate_angels_of_sun(latitude, longitude, timearray)
 
     # create excel
     solar_power = pd.DataFrame(columns=["RadiationNorth", "RadiationEast", "RadiationSouth", "RadiationWest"])
@@ -99,8 +107,8 @@ if __name__ == "__main__":
     solar_power["RadiationWest"] = E_west
     solar_power.to_csv("C:\\Users\\mascherbauer\\PycharmProjects\\NewTrends\\Prosumager\\_Philipp\\inputdata\\directRadiation_himmelsrichtung.csv", sep=";")
 
-    plt_anfang = 3000
-    plt_ende = 3024
+    plt_anfang = 0
+    plt_ende = 24
     x_achse = np.arange(plt_ende-plt_anfang)
     plt.plot(x_achse, E_sued[plt_anfang:plt_ende], "o", label="sued")
     plt.plot(x_achse, E_west[plt_anfang:plt_ende], "x", label="west")
@@ -108,6 +116,7 @@ if __name__ == "__main__":
     plt.plot(x_achse, E_nord[plt_anfang:plt_ende], "x", label="nord")
     plt.plot(x_achse, E_dir[plt_anfang:plt_ende], "x", label="E_dir")
     plt.legend()
+    plt.grid()
     plt.show()
 
 
