9ac3504cbfc999482a058e0e67de2ca5f73543e0
thohau <thomashaupt95@gmail.com>
2021-07-06 21:56:58 +0200

    Fixed plots: colors, yxis and DHW in demand profile

--- run_Operation.py
+++ run_Operation.py
@@ -3,7 +3,8 @@ from A_Infrastructure.A1_CONS import CONS
 from A_Infrastructure.A2_DB import DB
 from C_Model_Operation.C1_REG import REG_Table, REG_Var
 from C_Model_Operation.C3_TableGenerator import TableGenerator
-from C_Model_Operation.C4a_OperationOptimization import OperationOptimization
+from C_Model_Operation.C4_OperationOptimization import OperationOptimization
+# from C_Model_Operation.C4a_OperationOptimization import OperationOptimization
 from C_Model_Operation.C5_Visualization import Visualization
 
 
@@ -11,7 +12,7 @@ if __name__ == "__main__":
 
     CONN = DB().create_Connection(CONS().RootDB)
     # TableGenerator(CONN).run()
-    # OperationOptimization(CONN).run()
-    # Visualization(CONN).run()
+    #OperationOptimization(CONN).run()
+    Visualization(CONN).run()
 
 
