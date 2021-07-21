6cee4be1f9deba1a3bcebed6ab0516cda0cf2136
thohau <thomashaupt95@gmail.com>
2021-07-21 16:57:07 +0200

    Vizualization of optimization potential for each part

--- run_Operation.py
+++ run_Operation.py
@@ -6,8 +6,8 @@ from C_Model_Operation.C3_TableGenerator import TableGenerator
 
 
 #from C_Model_Operation.C4_RQ2_Reference_OperationOptimization import OperationOptimization
-#from C_Model_Operation.C4_RQ3_Battery_and_Optimization import OperationOptimization
-from C_Model_Operation.C4_RQ4_OperationOptimization_EV import OperationOptimization
+from C_Model_Operation.C4_RQ3_Battery_and_Optimization import OperationOptimization
+#from C_Model_Operation.C4_RQ4_OperationOptimization_EV import OperationOptimization
 
 #from C_Model_Operation.C4_OperationOptimization import OperationOptimization
 
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     CONN = DB().create_Connection(CONS().RootDB)
     #TableGenerator(CONN).run()
     #OperationOptimization(CONN).run()
-    Visualization(CONN).run()
+    #Visualization(CONN).run()
 
 
 
