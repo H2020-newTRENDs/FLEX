dd3fdb73c82d7a64742c6f697e872f68ecf64eb0
PhilippMaschi <phil.masch@hotmail.com>
2021-08-11 15:53:19 +0200

    C3_TableGenerator.py  gen_OBJ_ID_PV PV_Power wird aus PV GIS erstellt für alle ID_PV_Types

--- C_Model_Operation/C3_TableGenerator.py
+++ C_Model_Operation/C3_TableGenerator.py
@@ -143,10 +143,73 @@ class TableGenerator:
         self.gen_OBJ_ID_Table_1To1(REG_Table().Gen_OBJ_ID_HotWater, HotWaterBoiler)
         return None
 
-    def gen_OBJ_ID_PV(self):
-        PV = DB().read_DataFrame(REG_Table().ID_PVType, self.Conn)
-        self.gen_OBJ_ID_Table_1To1(REG_Table().Gen_OBJ_ID_PV, PV)
-        return None
+    def gen_OBJ_ID_PV(self, nuts_id):
+        # TODO rewrite function in case of more nuts ids
+        def get_JRC(lat, lon, startyear, endyear, peakpower):
+            # % JRC data
+            # possible years are 2005 to 2017
+            pvCalculation = 1  # 0 for no and 1 for yes
+            peakPower = peakpower  # kWp
+            pvLoss = 14  # system losses in %
+            pvTechChoice = "crystSi"  # Choices are: "crystSi", "CIS", "CdTe" and "Unknown".
+            trackingtype = 0  # Type of suntracking used, 0=fixed, 1=single horizontal axis aligned north-south,
+            # 2=two-axis tracking, 3=vertical axis tracking, 4=single horizontal axis aligned east-west,
+            # 5=single inclined axis aligned north-south.
+            optimalInclination = 1  # Calculate the optimum inclination angle. Value of 1 for "yes".
+            # All other values (or no value) mean "no". Not relevant for 2-axis tracking.
+            optimalAngles = 1  # Calculate the optimum inclination AND orientation angles. Value of 1 for "yes".
+            # All other values (or no value) mean "no". Not relevant for tracking planes.
+
+            req = f"https://re.jrc.ec.europa.eu/api/seriescalc?lat={lat}&" \
+                  f"lon={lon}&" \
+                  f"startyear={startyear}&" \
+                  f"endyear={endyear}&" \
+                  f"pvcalculation={pvCalculation}&" \
+                  f"peakpower={peakPower}&" \
+                  f"loss={pvLoss}&" \
+                  f"pvtechchoice={pvTechChoice}&" \
+                  f"components={1}&" \
+                  f"trackingtype={trackingtype}&" \
+                  f"optimalinclination={optimalInclination}&" \
+                  f"optimalangles={optimalAngles}"
+
+            # read the csv from api and set column names to list of 20 because depending on input parameters the number
+            # of rows will vary. This way all parameters are included for sure, empty rows are dropped afterwards:
+            df = pd.read_csv(req, sep=",", header=None, names=range(20)).dropna(how="all", axis=1)
+            # drop rows with nan:
+            df = df.dropna().reset_index(drop=True)
+            # set header to first column
+            header = df.iloc[0]
+            df = df.iloc[1:, :]
+            df.columns = header
+            df = df.reset_index(drop=True)
+            PV_Profile = pd.to_numeric(df["P"]).to_numpy()
+            return PV_Profile
+
+        startyear = DB().read_DataFrame(REG_Table().Sce_ID_TimeStructure, self.Conn).ID_Year.iloc[0]
+        endyear = DB().read_DataFrame(REG_Table().Sce_ID_TimeStructure, self.Conn).ID_Year.iloc[-1]
+        lat, lon = self.getNutsCenter(nuts_id)
+        ID_Hour = DB().read_DataFrame(REG_Table().Sce_ID_TimeStructure, self.Conn).ID_Hour.to_numpy()
+
+        PV_options = DB().read_DataFrame(REG_Table().ID_PVType, self.Conn)
+        columnNames = ["ID_Country", "ID_PVType", "ID_Hour", "PVPower", "Unit"]
+        TargetTable = np.zeros((1, len(columnNames)))
+        id_country = np.full((len(ID_Hour),), nuts_id)
+        unit = np.full((len(ID_Hour),), "kW")
+        for index, peakPower in enumerate(PV_options.PVPower):
+            # for peakpower = 0 exception:
+            if peakPower == 0:
+                PVProfile = np.full((len(ID_Hour),), 0)
+            else:
+                PVProfile = get_JRC(lat, lon, startyear, endyear, peakPower)
+            id_pv_type = np.full((len(PVProfile),), PV_options.ID_PVType[index])
+            table = np.column_stack([id_country, id_pv_type, ID_Hour, PVProfile, unit])
+            TargetTable = np.vstack([TargetTable, table])
+        # drop first column of TargetTable because its 0 from np.zeros:
+        TargetTable = TargetTable[1:, :]
+
+        DB().write_DataFrame(TargetTable, REG_Table().Gen_Sce_PhotovoltaicProfile, columnNames, self.Conn)
+
 
     def gen_OBJ_ID_Battery(self):
         Battery = DB().read_DataFrame(REG_Table().ID_BatteryType, self.Conn)
@@ -360,6 +423,31 @@ class TableGenerator:
 
         DB().write_DataFrame(TargetTable, REG_Table().Gen_Sce_HeatPump_HourlyCOP, TargetTable_columns, self.Conn)
 
+
+    def getNutsCenter(self, nuts_id,
+                      url_nuts0_poly="https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_3035_LEVL_0.geojson",
+                      url_nuts1_poly="https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_3035_LEVL_1.geojson",
+                      url_nuts2_poly="https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_3035_LEVL_2.geojson",
+                      url_nuts3_poly="https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_3035_LEVL_3.geojson"):
+        """
+        This function returns the latitude and longitude of the center of a NUTS region.
+        """
+        nuts_id = nuts_id.strip()
+        if len(nuts_id) == 2:
+            url = url_nuts0_poly
+        elif len(nuts_id) == 3:
+            url = url_nuts1_poly
+        elif len(nuts_id) == 4:
+            url = url_nuts2_poly
+        elif len(nuts_id) > 4:
+            url = url_nuts3_poly
+        else:
+            assert False, f"Error could not identify nuts level for {nuts_id}"
+        nuts = gpd.read_file(url)
+        transformer = Transformer.from_crs(CRS("EPSG:3035"), CRS("EPSG:4326"))
+        point = nuts[nuts.NUTS_ID == nuts_id].centroid.values[0]
+        return transformer.transform(point.y, point.x)  # returns lat, lon
+
     def gen_SolarRadiation_windows_and_outsideTemperature(self, nuts_id):  # TODO write for loop for multiple nuts IDs!
         """
         This function gets the solar radiation for every cilestial direction (North, East, West, South) on a vertical
@@ -379,32 +467,7 @@ class TableGenerator:
 
         The output is Sce_Weather_Temperature and Gen_Sce_Weather_Radiation_SkyDirections in the SQLite Database.
         """
-
-        def getNutsCenter(nuts_id,
-                          url_nuts0_poly="https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_3035_LEVL_0.geojson",
-                          url_nuts1_poly="https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_3035_LEVL_1.geojson",
-                          url_nuts2_poly="https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_3035_LEVL_2.geojson",
-                          url_nuts3_poly="https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2021_3035_LEVL_3.geojson"):
-            """
-            This function returns the latitude and longitude of the center of a NUTS region.
-            """
-            nuts_id = nuts_id.strip()
-            if len(nuts_id) == 2:
-                url = url_nuts0_poly
-            elif len(nuts_id) == 3:
-                url = url_nuts1_poly
-            elif len(nuts_id) == 4:
-                url = url_nuts2_poly
-            elif len(nuts_id) > 4:
-                url = url_nuts3_poly
-            else:
-                assert False, f"Error could not identify nuts level for {nuts_id}"
-            nuts = gpd.read_file(url)
-            transformer = Transformer.from_crs(CRS("EPSG:3035"), CRS("EPSG:4326"))
-            point = nuts[nuts.NUTS_ID == nuts_id].centroid.values[0]
-            return transformer.transform(point.y, point.x)  # returns lat, lon
-
-        def get_JRC(aspect, startyear, endyear):
+        def get_JRC(lat, lon, aspect, startyear, endyear):
             # % JRC data
             # possible years are 2005 to 2017
             pvCalculation = 0  # 0 for no and 1 for yes
@@ -435,6 +498,7 @@ class TableGenerator:
                   f"optimalangles={optimalAngles}&" \
                   f"angle={angle}&" \
                   f"aspect={aspect}"
+
             # read the csv from api and set column names to list of 20 because depending on input parameters the number
             # of rows will vary. This way all parameters are included for sure, empty rows are dropped afterwards:
             df = pd.read_csv(req, sep=",", header=None, names=range(20)).dropna(how="all", axis=1)
@@ -448,25 +512,25 @@ class TableGenerator:
 
         startyear = DB().read_DataFrame(REG_Table().Sce_ID_TimeStructure, self.Conn).ID_Year.iloc[0]
         endyear = DB().read_DataFrame(REG_Table().Sce_ID_TimeStructure, self.Conn).ID_Year.iloc[-1]
-        lat, lon = getNutsCenter(nuts_id)
+        lat, lon = self.getNutsCenter(nuts_id)
         # CelestialDirections are "north", "south", "east", "west":
         # Orientation (azimuth) angle of the (fixed) plane, 0=south, 90=west, -90=east. Not relevant for tracking planes
         for CelestialDirection in ["south", "east", "west", "north"]:
             if CelestialDirection == "south":
                 aspect = 0
-                df_south = get_JRC(aspect, startyear, endyear)
+                df_south = get_JRC(lat, lon, aspect, startyear, endyear)
                 south_radiation = pd.to_numeric(df_south["Gb(i)"]) + pd.to_numeric(df_south["Gd(i)"])
             elif CelestialDirection == "east":
                 aspect = -90
-                df_east = get_JRC(aspect, startyear, endyear)
+                df_east = get_JRC(lat, lon, aspect, startyear, endyear)
                 east_radiation = pd.to_numeric(df_east["Gb(i)"]) + pd.to_numeric(df_east["Gd(i)"])
             elif CelestialDirection == "west":
                 aspect = 90
-                df_west = get_JRC(aspect, startyear, endyear)
+                df_west = get_JRC(lat, lon, aspect, startyear, endyear)
                 west_radiation = pd.to_numeric(df_west["Gb(i)"]) + pd.to_numeric(df_west["Gd(i)"])
             elif CelestialDirection == "north":
                 aspect = -180
-                df_north = get_JRC(aspect, startyear, endyear)
+                df_north = get_JRC(lat, lon, aspect, startyear, endyear)
                 north_radiation = pd.to_numeric(df_north["Gb(i)"]) + pd.to_numeric(df_north["Gd(i)"])
 
         ID_Timestructure = DB().read_DataFrame(REG_Table().Sce_ID_TimeStructure, self.Conn)
@@ -595,4 +659,5 @@ if __name__ == "__main__":
     # A.gen_SolarRadiation_windows_and_outsideTemperature(nuts_id=NUTS_ID)
 
     # A.gen_Sce_HeatPump_HourlyCOP()  # is dependent on gen_SolarRadiation_windows_and_outsideTemperature
-    A.gen_sce_indoor_temperature()  # is dependent on gen_SolarRadiation_windows_and_outsideTemperature
+    # A.gen_sce_indoor_temperature()  # is dependent on gen_SolarRadiation_windows_and_outsideTemperature
+    A.gen_OBJ_ID_PV(NUTS_ID)
