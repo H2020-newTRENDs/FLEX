9392ba9d767c1b75e2b8672966b494e6c525bf0d
PhilippMaschi <phil.masch@hotmail.com>
2021-10-11 16:22:11 +0200

    add_DataFrame in A2_DB.py created

--- A_Infrastructure/A2_DB.py
+++ A_Infrastructure/A2_DB.py
@@ -1,5 +1,7 @@
 import sqlite3
 import pandas as pd
+import pandas.io.sql
+
 from A_Infrastructure.A1_CONS import CONS
 
 
@@ -43,9 +45,28 @@ class DB:
             table_DataFrame.to_sql(table_name, conn, index=False, if_exists='replace', chunksize=1000)
         return None
 
-    def add_to_dataframe(self, row, table_name, column_names, conn):
+    def add_DataFrame(self, table, table_name, column_names, conn, **kwargs):
+        table_DataFrame = pd.DataFrame(table, columns=column_names)
+        # check if table exists (1=yes, 0=no):
+        exists = pd.read_sql("SELECT count(*) FROM sqlite_master WHERE type='table' AND NAME ='{}'".format(table_name),
+                             con=conn)
+        if exists == 1:
+            sql_column_names = pd.read_sql("select * from " + table_name + " ORDER BY ROWID ASC LIMIT 1", con=conn)
+            column_names_sql = sql_column_names.columns
+            if len(column_names_sql) != len(table.columns):
+                raise Exception("number of parameters provided don't match column number of table")
+            for i in range(len(column_names)):
+                if column_names[i] != column_names_sql[i]:
+                    raise Exception("column names are not the same")
+            append_or_replace = "append"
+        elif exists == 0:  # if table does not exist jet, it is being created
+            append_or_replace = "replace"
 
-        pass
+        if "dtype" in kwargs:
+            table_DataFrame.to_sql(table_name, conn, index=False, if_exists=append_or_replace, chunksize=1000,
+                                   dtype=kwargs["dtype"])
+        else:
+            table_DataFrame.to_sql(table_name, conn, index=False, if_exists=append_or_replace, chunksize=1000)
 
     def revise_DataType(self, table_name, data_type, conn):
         table_DataFrame = self.read_DataFrame(table_name, conn)
@@ -56,3 +77,7 @@ class DB:
         table = self.read_DataFrame(table_name_from, conn_from)
         table.to_sql(table_name_to, conn_to, index=False, if_exists='replace', chunksize=1000)
         return None
+
+if __name__=="__main__":
+
+    DB().add_DataFrame(table, table_name, column_names, conn)
