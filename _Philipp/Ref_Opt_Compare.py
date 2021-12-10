b349851999dd7588a79b2ae5933aa786981a56d5
PhilippMaschi <phil.masch@hotmail.com>
2021-12-10 12:25:38 +0100

    Ref_Opt_Compare.py: starting to play with super()

--- _Philipp/Ref_Opt_Compare.py
+++ _Philipp/Ref_Opt_Compare.py
@@ -13,10 +13,28 @@ from A_Infrastructure.A1_CONS import CONS
 from C_Model_Operation.C4_OperationOptimization_paper import DataSetUp, create_abstract_model, update_instance, \
     create_instances2solve
 from C_Model_Operation.C4_NoOptimization import no_DR
+from C_Model_Operation.C2_DataCollector import DataCollector
 
 
-class HouseholdToTest:
+def check_dict_exists(table_name) -> bool:
+    """checks if the dict exists in sqlite db. returns True if table exists and False if not"""
+    c = DB().create_Connection(CONS().RootDB).cursor()
+    # get the count of tables with the name
+    exists = pd.read_sql(
+        "SELECT count(*) FROM sqlite_master WHERE type='table' AND NAME ='{}'".format(table_name),
+        con=DB().create_Connection(CONS().RootDB))
+    # if the count is 1, then table exists
+    if exists.iloc[0].iloc[0] == 1:
+        print('Table exists.')
+        # close the connection
+        return True
+    else:
+        print('Table does not exist.')
+        # close the connection
+        return False
 
+
+class DefineTestHouse:
     def __init__(self):
         self.id_chosen = {}
         self.conn = DB().create_Connection(CONS().RootDB)
@@ -39,6 +57,11 @@ class HouseholdToTest:
             return str(output_value)
 
         for frame_name, frame in frame_dict.items():
+            # check if frame has less than 3 columns:
+            if frame.shape[1] < 3:
+                frame[""] = ""  # appending an empty column at the end
+            else:
+                pass
             # create tkinter root
             root = tk.Tk()
             # title
@@ -89,29 +112,12 @@ class HouseholdToTest:
             enter_button.grid(row=3 + row_number + 1, column=0, columnspan=2)
             root.mainloop()
 
-    def check_dict_exists(self) -> bool:
-        """checks if the dict exists in sqlite db. returns True if table exists and False if not"""
-        c = self.conn.cursor()
-        # get the count of tables with the name
-        exists = pd.read_sql(
-            "SELECT count(*) FROM sqlite_master WHERE type='table' AND NAME ='{}'".format(self.test_house),
-            con=self.conn)
-        # if the count is 1, then table exists
-        if exists.iloc[0].iloc[0] == 1:
-            print('Table exists.')
-            # close the connection
-            return True
-        else:
-            print('Table does not exist.')
-            # close the connection
-            return False
-
-    def define_household_IDs(self, new_id: bool = False) -> pd.DataFrame:
+    def define_test_household_IDs(self, new_id: bool = False) -> pd.DataFrame:
         """defines the household used for the comparison and returns the IDs as Dataframe
         if new_id = False (default) the IDs are loaded from the database if they exist
         if new_id = True the IDs will be newly generated through user input"""
         # check if sqlite file exists:
-        if self.check_dict_exists() and not new_id:
+        if check_dict_exists(self.test_house) and not new_id:
             # read the dictionary from database
             test_IDs_df = DB().read_DataFrame(table_name=self.test_house, conn=self.conn)
         else:  # household IDs will be determined by user input
@@ -132,18 +138,21 @@ class HouseholdToTest:
                                                       *["ID", "SpaceCoolingPower", "SpaceCoolingPower_unit"])
             battery_table = DB().read_DataFrame(REG_Table().Gen_OBJ_ID_Battery, self.conn,
                                                 *["ID_BatteryType", "Capacity", "Capacity_unit"])
+            age_group_table = DB().read_DataFrame(REG_Table().ID_AgeGroup, self.conn)
 
             list_of_frames = {"ID_PV": PV_table,
                               "ID_Building": building_table,
-                              "ID_SpaceHeating": space_cooling_table,
-                              "ID_SpaceCooling": space_heating_table,
+                              "ID_SpaceHeating": space_heating_table,
+                              "ID_SpaceCooling": space_cooling_table,
                               "ID_Battery": battery_table,
+                              "ID_AgeGroup": age_group_table,
                               "ID_Environment": electricity_price_table}
 
             # get the user input:
             self.choose_household_tkinter(list_of_frames)
             # save user input to sqlite
-            test_IDs_df = pd.DataFrame.from_dict(self.id_chosen, orient="index", columns=["ID"]).reset_index()  # convert dict to Dataframe
+            test_IDs_df = pd.DataFrame.from_dict(self.id_chosen, orient="index",
+                                                 columns=["ID"]).reset_index()  # convert dict to Dataframe
             DB().write_DataFrame(table=test_IDs_df,
                                  table_name=self.test_house,
                                  conn=self.conn,
@@ -153,44 +162,192 @@ class HouseholdToTest:
         return test_IDs_df
 
 
-class TestingModelModes:
-    """if new_id = False (default) the IDs are loaded from the database if they exist
-        if new_id = True the IDs will be newly generated through user input"""
-    def __init__(self, new_id: bool = False):
+class RunModelModes:
+    """if new_id = True (default) the IDs will be newly generated through user input and the
+        data will be newly generated and saved to the DB
+        if new_id = False the IDs are loaded from the database if they exist as well as the results"""
+
+    def __init__(self, new_id: bool = True):
         self.conn = DB().create_Connection(CONS().RootDB)
-        self.testing_ids = HouseholdToTest().define_household_IDs(new_id)
+        self.new_id = new_id
+        self.testing_ids = DefineTestHouse().define_test_household_IDs(new_id).set_index("index", drop=True)
+        self.Gen_OBJ_ID_Household = DB().read_DataFrame(REG_Table().Gen_OBJ_ID_Household, self.conn)
+        self.Gen_Sce_ID_Environment = DB().read_DataFrame(REG_Table().Gen_Sce_ID_Environment, self.conn)
+        self.table_name_reference_yearly = "test_results_reference_yearly"
+        self.table_name_reference_hourly = "test_results_reference_hourly"
+        self.table_name_optimization_yearly = "test_results_optimization_yearly"
+        self.table_name_optimization_hourly = "test_results_optimization_hourly"
 
+    def get_row_ids_optimization(self) -> (int, int):
+        """gets the row id for the specific testing_ids in the Gen_OBJ_ID_Household frame and
+        the row id for the environment ID"""
+        single_row_id = self.Gen_OBJ_ID_Household.loc[
+                            (self.Gen_OBJ_ID_Household.loc[:, "ID_PV"] == self.testing_ids.loc["ID_PV"].values[0]) &
+                            (self.Gen_OBJ_ID_Household.loc[:, "ID_Building"] ==
+                             self.testing_ids.loc["ID_Building"].values[0]) &
+                            (self.Gen_OBJ_ID_Household.loc[:, "ID_SpaceHeating"] ==
+                             self.testing_ids.loc["ID_SpaceHeating"].values[0]) &
+                            (self.Gen_OBJ_ID_Household.loc[:, "ID_SpaceCooling"] ==
+                             self.testing_ids.loc["ID_SpaceCooling"].values[0]) &
+                            (self.Gen_OBJ_ID_Household.loc[:, "ID_Battery"] ==
+                             self.testing_ids.loc["ID_Battery"].values[0]) &
+                            (self.Gen_OBJ_ID_Household.loc[:, "ID_AgeGroup"] ==
+                             self.testing_ids.loc["ID_AgeGroup"].values[0])
+                            ]["ID"].values[0] - 1  # minus 1 because counting starts at 0
 
-    def extract_Result2Array(self, result_DictValues) -> np.array:
-        """extracts the results from the pyomo instance and returns the results as np.array"""
-        result_array = np.nan_to_num(np.array(list(result_DictValues.values()), dtype=np.float), nan=0)
-        return result_array
+        single_environment_id = self.Gen_Sce_ID_Environment.loc[
+                                    self.Gen_Sce_ID_Environment.loc[:, "ID"] ==
+                                    self.testing_ids.loc["ID_Environment"].values[0]
+                                    ]["ID"].values[0] - 1  # minus 1 because counting starts at 0
+        return single_row_id, single_environment_id
 
-    # DB().read_DataFrame(table_name=REG_Table.Gen_OBJ_ID_Household,
-    #                     conn=self.conn,
-    #                     ID_Building= ,
-    #                     ID_SpaceHeating= ,
-    #                     ID_SpaceCooling= ,
-    #                     ID_PV= ,
-    #                     ID_Battery= )
+    def get_row_ids_reference(self) -> (int, int):
+        """gets the row id for the specific testing_ids in the REDUCED Gen_OBJ_ID_Household frame and
+        the row id for the environment ID"""
+        reduced_gen_obj_id_household = no_DR().remove_household_IDs()
+        single_row_id = reduced_gen_obj_id_household.loc[
+                            (reduced_gen_obj_id_household.loc[:, "ID_PV"] == self.testing_ids.loc["ID_PV"].values[0]) &
+                            (reduced_gen_obj_id_household.loc[:, "ID_SpaceHeating"] ==
+                             self.testing_ids.loc["ID_SpaceHeating"].values[0]) &
+                            (reduced_gen_obj_id_household.loc[:, "ID_SpaceCooling"] ==
+                             self.testing_ids.loc["ID_SpaceCooling"].values[0]) &
+                            (reduced_gen_obj_id_household.loc[:, "ID_Battery"] ==
+                             self.testing_ids.loc["ID_Battery"].values[0]) &
+                            (reduced_gen_obj_id_household.loc[:, "ID_AgeGroup"] ==
+                             self.testing_ids.loc["ID_AgeGroup"].values[0])
+                            ]["ID"].values[0] - 1  # minus 1 because counting starts at 0
 
-    def run_reference(self) -> np.array:
+        single_environment_id = self.Gen_Sce_ID_Environment.loc[
+                                    self.Gen_Sce_ID_Environment.loc[:, "ID"] ==
+                                    self.testing_ids.loc["ID_Environment"].values[0]
+                                    ]["ID"].values[0] - 1  # minus 1 because counting starts at 0
+        return single_row_id, single_environment_id
+
+    def run_reference(self) -> (np.array, np.array):
         """runs the reference calculation and returns the hourly values as numpy array"""
+        household_row_id, environment_row_id = self.get_row_ids_reference()
+        yearly_results_all_ids, hourly_results_all_ids = no_DR().calculate_noDR(household_row_id, environment_row_id)
+        # ref model calculates for all building IDs at the same time -> take out single building ID
+        yearly_results = yearly_results_all_ids[yearly_results_all_ids[:, 1] ==
+                                                str(self.testing_ids.loc["ID_Building"].values[0]),
+                         :]  # column 1 is building ID
+        hourly_results = hourly_results_all_ids[hourly_results_all_ids[:, 1] ==
+                                                str(self.testing_ids.loc["ID_Building"].values[0]), :]
+        return yearly_results, hourly_results
 
-    def run_optimization(self):
-        """runs the optimization and returns the hourly results as pyomo instance opject"""
+    def run_optimization(self) -> (np.array, np.array):
+        """runs the optimization and returns the yearly and hourly results as numpy array"""
         # model input data
         DC = DataCollector()
         Opt = pyo.SolverFactory("gurobi")
-        input_data_initial = DataSetUp().get_input_data(0, 0)
-        initial_parameters = input_data_initial["input_parameters"]
+        # get household and environment row ids or set them new
+        household_row_id, environment_row_id = self.get_row_ids_optimization()
+        input_data = DataSetUp().get_input_data(household_row_id, environment_row_id)
+        initial_parameters = input_data["input_parameters"]
         # create the instance once:
         model = create_abstract_model()
         pyomo_instance = model.create_instance(data=initial_parameters)
+        instance2solve = update_instance(input_data, pyomo_instance)
+        # solve model
+        result = Opt.solve(instance2solve, tee=False)
+        yearly_results, hourly_results = DC.collect_OptimizationResult(
+            input_data["Household"], input_data["Environment"], instance2solve)
+        return yearly_results, hourly_results
+
+    def get_results_both_modes(self) -> (np.array, np.array, np.array, np.array):
+        """returns the hourly and yearly results of the reference and the optimization mode as numpy arrays
+        results are returned as follows:
+        yearly-reference, hourly-reference, yearly-optimization, hourly-optimization"""
+        if self.new_id:
+            # calculate the results, save and return them
+            yearly_results_reference, hourly_results_reference = self.run_reference()
+            yearly_results_optimization, hourly_results_optimization = self.run_optimization()
+            # save
+            self.save_results(yearly_results_reference, hourly_results_reference, yearly_results_optimization,
+                              hourly_results_optimization)
+            return yearly_results_reference, hourly_results_reference, \
+                   yearly_results_optimization, hourly_results_optimization
+        else:
+            # if new_id is False, check if results are in DB:
+            # if yes, load them and return them,
+            if check_dict_exists(self.table_name_optimization_hourly) \
+                    and check_dict_exists(self.table_name_optimization_yearly) \
+                    and check_dict_exists(self.table_name_reference_hourly) \
+                    and check_dict_exists(self.table_name_reference_yearly):
+                # load results
+                yearly_results_reference = DB().read_DataFrame(self.table_name_reference_yearly, self.conn)
+                hourly_results_reference = DB().read_DataFrame(self.table_name_reference_hourly, self.conn)
+                yearly_results_optimization = DB().read_DataFrame(self.table_name_optimization_yearly, self.conn)
+                hourly_results_optimization = DB().read_DataFrame(self.table_name_optimization_hourly, self.conn)
+                return yearly_results_reference, hourly_results_reference, \
+                       yearly_results_optimization, hourly_results_optimization
+
+            # if no, calculate them, save them and return them:
+            else:
+                # calculate
+                yearly_results_reference, hourly_results_reference = self.run_reference()
+                yearly_results_optimization, hourly_results_optimization = self.run_optimization()
+                # save
+                self.save_results(yearly_results_reference, hourly_results_reference, yearly_results_optimization,
+                                  hourly_results_optimization)
+                # return
+                return yearly_results_reference, hourly_results_reference, \
+                       yearly_results_optimization, hourly_results_optimization
+
+    def save_results(self, yearly_results_reference, hourly_results_reference, yearly_results_optimization,
+                     hourly_results_optimization):
+        """save results to DB (mainly for developing the plots so I dont have to wait for every run to be finished)"""
+        # reference year
+        DB().write_DataFrame(table=yearly_results_reference,
+                             table_name=self.table_name_reference_yearly,
+                             column_names=DataCollector(self.conn).SystemOperationYear_Column.keys(),
+                             conn=self.conn,
+                             dtype=DataCollector(self.conn).SystemOperationYear_Column)
+        # reference hourly
+        DB().write_DataFrame(table=hourly_results_reference,
+                             table_name=self.table_name_reference_hourly,
+                             column_names=DataCollector(self.conn).SystemOperationHour_Column.keys(),
+                             conn=self.conn,
+                             dtype=DataCollector(self.conn).SystemOperationHour_Column)
+        # optimization year
+        DB().write_DataFrame(table=yearly_results_optimization,
+                             table_name=self.yearly_results_optimization,
+                             column_names=DataCollector(self.conn).SystemOperationYear_Column.keys(),
+                             conn=self.conn,
+                             dtype=DataCollector(self.conn).SystemOperationYear_Column)
+        # optimization hourly
+        DB().write_DataFrame(table=hourly_results_optimization,
+                             table_name=self.hourly_results_optimization,
+                             column_names=DataCollector(self.conn).SystemOperationHour_Column.keys(),
+                             conn=self.conn,
+                             dtype=DataCollector(self.conn).SystemOperationHour_Column)
+
+
+class CompareModelModes(RunModelModes):
+    def __init__(self, new_id: bool = False):
+        super().__init__(new_id)
+        self.conn = DB().create_Connection(CONS().RootDB)
+        self.yearly_results_reference = None
+        self.hourly_results_reference = None
+        self.yearly_results_optimization = None
+        self.hourly_results_optimization = None
+        self.table_name_reference_yearly = "test_results_reference_yearly"
+        self.table_name_reference_hourly = "test_results_reference_hourly"
+        self.table_name_optimization_yearly = "test_results_optimization_yearly"
+        self.table_name_optimization_hourly = "test_results_optimization_hourly"
+
+    def calculate_results(self):
+        """runs the optimization and reference mode and saves results to self-variables"""
+        yearly_results_reference, hourly_results_reference, yearly_results_optimization, hourly_results_optimization = \
+            RunModelModes(new_id=self.new_id).get_results_both_modes()
+        self.yearly_results_reference = yearly_results_reference
+        self.hourly_results_reference = hourly_results_reference
+        self.yearly_results_optimization = yearly_results_optimization
+        self.hourly_results_optimization = hourly_results_optimization
 
     def plot_comparison_results(self):
         """visualizes the results from both modes for the whole year and hourly for certain weeks"""
 
 
 if __name__ == "__main__":
-    HouseholdToTest().define_household_IDs()
+    RunModelModes(new_id=True).get_results_both_modes()
