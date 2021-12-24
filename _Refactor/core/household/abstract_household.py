3482df1c4a217e38f5bbcc2830adc363cbe807e3
SongminYu <yu.ceepcas@gmail.com>
2021-12-24 12:26:18 +0100

    added a new "hot_water_tank" for household, which is separated from "space_heating_tank".

--- _Refactor/core/household/abstract_household.py
+++ _Refactor/core/household/abstract_household.py
@@ -4,8 +4,8 @@ from abc import ABC, abstractmethod
 
 from _Refactor.core.elements.pillar import Pillar
 from _Refactor.core.scenario.abstract_scenario import AbstractScenario
-from _Refactor.core.household.components import Region, PersonList, Building, ApplianceList, Boiler, Tank,\
-                                                AirConditioner, PV, Battery, Vehicle, Behavior, Demand
+from _Refactor.core.household.components import Region, PersonList, Building, ApplianceList, Boiler, SpaceHeatingTank,\
+                                                AirConditioner, HotWaterTank, PV, Battery, Vehicle, Behavior, Demand
 
 
 class AbstractHousehold(Pillar):
@@ -16,8 +16,9 @@ class AbstractHousehold(Pillar):
         self.building = None
         self.appliance_list = None
         self.boiler = None
-        self.tank = None
+        self.space_heating_tank = None
         self.air_conditioner = None
+        self.hot_water_tank = None
         self.pv = None
         self.battery = None
         self.vehicle = None
@@ -30,8 +31,9 @@ class AbstractHousehold(Pillar):
         self.building_class: ClassVar['Building'] = Building
         self.appliance_list_class: ClassVar['ApplianceList'] = ApplianceList
         self.boiler_class: ClassVar['Boiler'] = Boiler
-        self.tank_class: ClassVar['Tank'] = Tank
+        self.space_heating_tank_class: ClassVar['SpaceHeatingTank'] = SpaceHeatingTank
         self.air_conditioner_class: ClassVar['AirConditioner'] = AirConditioner
+        self.hot_water_tank_class: ClassVar['HotWaterTank'] = HotWaterTank
         self.pv_class: ClassVar['PV'] = PV
         self.battery_class: ClassVar['Battery'] = Battery
         self.vehicle_class: ClassVar['Vehicle'] = Vehicle
