7d9f38fc922d6fc8256d15282042c7481191bc96
SongminYu <yu.ceepcas@gmail.com>
2022-01-05 15:37:02 +0100

    data collector

---
+++ _Refactor/models/operation/data_collector.py
@@ -0,0 +1,7 @@
+
+from _Refactor.core.elements.data_collector import DataCollector
+
+class OperationDataCollector(DataCollector):
+
+    def setup(self):
+        self.add_var("var_name", var_data_type)
