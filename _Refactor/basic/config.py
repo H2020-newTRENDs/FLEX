0644f8525023d0ede5de0506489a04e7a2deb2c2
PhilippMaschi <phil.masch@hotmail.com>
2022-01-07 12:29:18 +0100

    db gets directory path for database plus name from config file

--- _Refactor/basic/config.py
+++ _Refactor/basic/config.py
@@ -5,9 +5,12 @@ class Config:
                  project_name: str,
                  project_root: str,
                  sqlite_folder: str,
+                 database_name: str,
                  output_folder: str
                  ):
         self.root_db_folder = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
+        self.root_db = os.path.join(os.path.dirname(os.path.dirname(__file__), sqlite_folder, database_name))
+        self.database_name = database_name
         self.project_name = project_name
         self.project_root = project_root
         self.sqlite_folder = sqlite_folder
---
+++ _Refactor/projects/PhilippTest/main.py
@@ -0,0 +1,16 @@
+
+import os
+
+from config import project_config
+from modules.model_opt import PhilippTestOptOperationModel
+from modules.environment import PhilippTestEnvironment
+from modules.household import PhilippTestHousehold
+from modules.scenario import PhilippTestScenario
+
+scenario = PhilippTestScenario(scenario_id=0, db_name="ProsumagerUpdated_Philipp")
+household = PhilippTestHousehold(scenario)
+environment = PhilippTestEnvironment(scenario)
+model = PhilippTestOptOperationModel(household, environment)
+
+print(model.household.space_heating_tank.loss)
+print(model.household.hot_water_tank.loss)
