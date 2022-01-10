ba0730a801c62342e84c1405a34be726a34825e4
PhilippMaschi <phil.masch@hotmail.com>
2022-01-10 10:01:31 +0100

    read_dataFrame to read_dataframe

--- _Refactor/basic/db.py
+++ _Refactor/basic/db.py
@@ -30,7 +30,7 @@ class DB:
         data_frame.to_sql(table_name, self.connection, index=False, dtype=data_types, if_exists=if_exists,
                           chunksize=10_000)
 
-    def read_dataFrame(self, table_name: str, conn, *column_names: list, **kwargs: dict) -> pd.DataFrame:
+    def read_dataframe(self, table_name: str, conn, *column_names: list, **kwargs: dict) -> pd.DataFrame:
         """column_names will only extract certain columns (list)
         kwargs have to be dict with: column_name: value. The table where the column has that value is returned"""
         if len(column_names) > 0:
