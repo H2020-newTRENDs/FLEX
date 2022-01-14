09ca8735c51d3eaa7ca20ef497a5955ab29526a9
PhilippMaschi <phil.masch@hotmail.com>
2022-01-14 18:54:47 +0100

    added vectors to electricity price and feed in tariff

--- _Refactor/core/household/components/electricity_price.py
+++ _Refactor/core/household/components/electricity_price.py
@@ -6,6 +6,8 @@ class ElectricityPrice(Component):
     def __init__(self, component_id):
         self.ID_ElectricityPrice: int = None
         self.name: str = None
+        self.electricity_price = None
+        self.unit = None
 
         self.set_parameters(component_id)
 
