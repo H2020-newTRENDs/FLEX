5916d1afd9bada50eb5d94cc9214c9815529be1e
PhilippMaschi <phil.masch@hotmail.com>
2022-01-14 18:42:36 +0100

    changed the envirnoment_components to components

--- _Refactor/core/environment/components/__init__.py
+++ _Refactor/core/environment/components/__init__.py
@@ -1,6 +1,2 @@
-from _Refactor.core.household.components.electricity_price import ElectricityPrice
-from _Refactor.core.household.components.feedin_tariff import FeedInTariff
 
-
-environment_component_list = [ElectricityPrice, FeedInTariff]
 
