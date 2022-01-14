dba34ad5cc63892d949c3bb8c105045564d274bd
PhilippMaschi <phil.masch@hotmail.com>
2022-01-14 17:19:04 +0100

    changed get_data in GeneralComponents so it also works for vectors (like electricity price)

--- _Refactor/core/elements/component_setup.py
+++ _Refactor/core/elements/component_setup.py
@@ -4,14 +4,16 @@ from typing import Dict, Any
 from os.path import join, dirname
 from pathlib import Path
 
+import pandas as pd
+
 from _Refactor.basic.db import DB
-from _Refactor.basic.config import Config
+import _Refactor.basic.config as config
 
 
 class GetComponentParamsDictStrategy(ABC):
 
     @abstractmethod
-    def get_params_dict(self) -> Dict[str, Any]:
+    def get_complete_data(self) -> Dict[str, Any]:
         pass
 
 
@@ -21,13 +23,17 @@ class GeneralComponent(GetComponentParamsDictStrategy):
                  row_id: int,
                  table_name: str,
                  db_name: str = "root",
-                 db_folder: str = Path(__file__).parent / Path("data")):
+                 db_folder: str = Path(__file__).parent.parent.parent / Path("data")):
+        data_base_connection = config.create_connection(db_folder=db_folder, db_name=db_name)
         self.table_name = table_name
         self.row_id = row_id
-        self.db = DB()
+        self.db = DB(data_base_connection)
 
-    def get_params_dict(self) -> Dict[str, Any]:
-        table = self.db.read_dataframe(self.table_name)
-        param_dict = table.iloc[self.row_id].to_dict()
-        return param_dict
+    def get_complete_data(self) -> pd.DataFrame:
+        # reads only the data where the ID is equal to the row ID. This way we can read in profiles with 8760 values
+        kwargs_dict = {"ID_" + self.table_name: self.row_id}
+        table = self.db.read_dataframe(self.table_name, **kwargs_dict)
+        return table
+
+
 
