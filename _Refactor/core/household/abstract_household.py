be427d4e13743a609deb793102109e404274123e
PhilippMaschi <phil.masch@hotmail.com>
2022-01-21 17:13:03 +0100

    abstract_household.py: included scenario_id

--- _Refactor/core/household/abstract_household.py
+++ _Refactor/core/household/abstract_household.py
@@ -3,13 +3,15 @@ from typing import Type, Optional, ClassVar, TYPE_CHECKING
 from abc import ABC, abstractmethod
 
 from _Refactor.core.elements.pillar import Pillar
-from _Refactor.core.scenario.abstract_scenario import AbstractScenario
 import _Refactor.core.household.components as components
+from _Refactor.core.elements.component_setup import GeneralComponent
+from _Refactor.basic.reg import Table
+
 
 
 class AbstractHousehold:
 
-    def __init__(self, scenario: 'AbstractScenario'):
+    def __init__(self, scenario_id: int):
         self.region_class = None
         self.person_class = None
         self.personlist_class = None
@@ -29,17 +31,21 @@ class AbstractHousehold:
         self.electricityprice_class = None
         self.feedintariff_class = None
 
-        self.scenario = scenario
+        # get all scenario ids from the scenarios table:
+        self.scenario_ids = GeneralComponent(
+            table_name=Table().scenarios,
+            row_id=scenario_id
+        ).get_complete_data()  # is a dictionary  with the ids
         self.add_component_classes()
 
     def add_component_classes(self):
         # iterate through the scenario dict which hols all IDs for each component:
-        for component, component_id in self.scenario.__dict__.items():
+        for component, component_id in self.scenario_ids.items():
             for key in components.__dict__.keys():
                 if component == key.lower():  # check if they class exists and use its name
                     component_name = key
             # get the class
-            print(component_name)
+            print(f"{component_name} exists")
             class_filled = getattr(components, component_name)(component_id=component_id)
             # create self variable of the class with the filled class
             setattr(self, component + "_class", class_filled)
@@ -63,6 +69,5 @@ class AbstractHousehold:
 
 
 if __name__ == "__main__":
-    scenario = AbstractScenario(scenario_id=0)
-    AbstractHousehold(scenario).add_component_classes()
+    AbstractHousehold(scenario_id=0).add_component_classes()
 
