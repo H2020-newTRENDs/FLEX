762c005de141dbc033a3173ebe0aa07e8a3260e9
PhilippMaschi <phil.masch@hotmail.com>
2022-01-28 19:08:26 +0100

    data_collector.py: data_collector for optimization (hourly and yearly) finished. Returns the dataframes with results. Data is not saved to DB yet

--- _Refactor/models/operation/data_collector.py
+++ _Refactor/models/operation/data_collector.py
@@ -9,14 +9,14 @@ class OptimizationDataCollector(MotherDataCollector):
     def __init__(self):
         pass
 
-    def extend_to_array(self, array_or_value: np.array):
+    def extend_to_array(self, array_or_value: np.array) -> np.array:
         # check if array_or_value is an array or a single value:
         if len(array_or_value) == 1:
             return np.full((8760, ), array_or_value)  # return array with same value
         else:
             return array_or_value
 
-    def collect_OptimizationResult(self, instance) -> (np.array, np.array):
+    def collect_optimization_results_hourly(self, instance) -> pd.DataFrame:
         # empty dataframe
         dataframe = pd.DataFrame()
         # iterate over all variables in the instance
@@ -35,14 +35,40 @@ class OptimizationDataCollector(MotherDataCollector):
             result_value = np.array(list(result_class.extract_values().values()))
             dataframe[result_name] = self.extend_to_array(result_value)
 
+        return dataframe
+
+    def collect_optimization_results_yearly(self, instance) -> pd.DataFrame:
+        # empty dictionary
+        dictionary = {}
+        # iterate over all variables in the instance
+        for result_name in instance.__dict__.keys():
+            if result_name.startswith("_") \
+                    or result_name == "doc" \
+                    or result_name == "statistics" \
+                    or result_name == "config" \
+                    or result_name == "solutions" \
+                    or result_name == "t":  # do not save pyomo implemented types and the time set
+                continue
+            elif "rule" in result_name:  # do not save rules
+                continue
+
+            result_class = getattr(instance, result_name)
+            result_array = np.array(list(result_class.extract_values().values()))
+            if len(result_array) > 1:  # if its hourly value, take sum
+                # check if the values in every hour are exactly the same (eg. fixed feed in tariff):
+                if np.all(result_array == result_array[0]):
+                    result_value = result_array[0]
+                else:  # else take the sum
+                    result_value = result_array.sum()
+            else:  # if its already a single value, just take that value
+                result_value = result_array
+            # add the value to the dictionary with its name:
+            dictionary[result_name] = result_value
+        # create the dataframe
+        dataframe = pd.DataFrame(dictionary)
+        return dataframe
 
 
 
 
-        ElectricityPrice_array = self.extract_Result2Array(
-            instance.electricity_price.extract_values()) * 1_000  # Cent/kWh
-        FeedinTariff_array = self.extract_Result2Array(instance.FiT.extract_values()) * 1_000  # Cent/kWh
-        OutsideTemperature_array = self.extract_Result2Array(instance.T_outside.extract_values())
-
-        E_BaseLoad_array = self.extract_Result2Array(instance.BaseLoadProfile.extract_values()) / 1000  # kW
 
