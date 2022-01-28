977411e89d21e1f2307024d6b992e9f11e6a563f
PhilippMaschi <phil.masch@hotmail.com>
2022-01-28 19:08:26 +0100

    data_collector.py: data_collector for optimization (hourly and yearly) finished. Returns the dataframes with results. Data is not saved to DB yet

--- _Refactor/models/operation/opt.py
+++ _Refactor/models/operation/opt.py
@@ -680,7 +680,7 @@ class OptOperationModel(AbstractOperationModel):
         return instance2solve
 
     def collect_results(self, solved_instance):
-        OptimizationDataCollector().collect_OptimizationResult(solved_instance)
+        OptimizationDataCollector().collect_optimization_results_hourly(solved_instance)
 
 
 
@@ -698,6 +698,8 @@ if __name__ == "__main__":
     # solve model
     solved_instance = model_class.solve_optimization(updated_instance)
     # datacollector
-    model_class.collect_results(solved_instance)
+    hourly_results_df = OptimizationDataCollector().collect_optimization_results_hourly(solved_instance)
+    yearly_results_df = OptimizationDataCollector().collect_optimization_results_yearly(solved_instance)
 
+    pass
 
