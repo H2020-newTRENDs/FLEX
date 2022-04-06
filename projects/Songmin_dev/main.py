dbd73ada6211d520fb646fe0bbf4cfe15b837c3c
thohau <thomashaupt95@gmail.com>
2022-04-06 16:01:14 +0200

    Start temp of building mass fixed to 10°C models run fault in € calc Model run successfully - no EV Infeasable for now

--- projects/Songmin_dev/main.py
+++ projects/Songmin_dev/main.py
@@ -1,6 +1,8 @@
 from flex.flex_operation.scenario import OperationScenario
 from config import config
 from flex.flex_operation_old.opt import OptOperationModel
+from flex.flex_operation_old.opt import OptimizationDataCollector
+
 
 
 scenario = OperationScenario(scenario_id=1, config=config)
@@ -13,13 +15,12 @@ y = scenario.energy_price.electricity_consumption
 y= scenario.boiler.db_name
 print(scenario.building.internal_gains)
 
-
-
-
-
-
 optimization_model = OptOperationModel(scenario)
 solved_instance = optimization_model.run()
+
+
+OptimizationDataCollector(solved_instance, scenario.scenario_id).save_yearly_results()
+OptimizationDataCollector(solved_instance, scenario.scenario_id).save_hourly_results()
 
 
 
