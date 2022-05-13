dcef8747cff86553168f3e9de4aca21a9cb82410
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2022-05-13 02:41:13 +0200

    project structure cleaned (bug in optimization)

---
+++ models/operation/enums.py
@@ -0,0 +1,40 @@
+from enum import Enum
+from models.operation import components
+
+
+class ScenarioEnum(Enum):
+    Scenario = "scenario"
+    Region = "region"
+    Building = "building"
+    Boiler = "boiler"  # TODO: this name is a bit confusing, better to be "SpaceHeatingTechnology"
+    SpaceHeatingTank = "space_heating_tank"
+    HotWaterTank = "hot_water_tank"
+    SpaceCoolingTechnology = "space_cooling_technology"
+    PV = "pv"
+    Battery = "battery"
+    Vehicle = "vehicle"
+    SEMS = "sems"
+    EnergyPrice = "energy_price"
+    Behavior = "behavior"
+
+    @property
+    def table_name(self):
+        if self.name == 'Scenario':
+            table_name = "Scenario"
+        else:
+            table_name = "Scenario_Component_" + self.name
+        return table_name
+
+    @property
+    def id(self):
+        return "ID_" + self.name
+
+    @property
+    def class_var(self):
+        return components.__dict__[self.name]
+
+
+class TableEnum(Enum):
+    BehaviorProfile = 'Scenario_Profile_Behavior'
+    EnergyPriceProfile = 'Scenario_Profile_EnergyPrice'
+    RegionWeatherProfile = 'Scenario_Profile_RegionWeather'
--- projects/Songmin_dev/config.py
+++
@@ -1,5 +0,0 @@
-from flex.core.config import Config
-
-flex_operation_config = {}
-config = Config(db_name="SongminDev",
-                flex_operation_config=flex_operation_config)
