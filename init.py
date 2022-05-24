2ff43752416480ae12601c21bd169f0d9e272410
PhilippMaschi <phil.masch@hotmail.com>
2022-05-24 13:38:15 +0200

    changed some settings in the input xlsx to make the model feasable! it is still infeasable with EV

--- init.py
+++ init.py
@@ -26,5 +26,5 @@ class ProjectDatabaseInitializer(DatabaseInitializer):
 
 
 if __name__ == "__main__":
-    init = ProjectDatabaseInitializer(config, scenario_enums=ScenarioEnum)
+    init = ProjectDatabaseInitializer(config=config, scenario_enums=ScenarioEnum)
     init.main()
