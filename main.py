db3224abdcaff5044e66761796d6da26a49efa70
PhilippMaschi <phil.masch@hotmail.com>
2022-05-25 15:05:43 +0200

    main.py: Added Ref_model to imports

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from models.operation.scenario import OperationScenario
 from models.operation.opt import OptOperationModel
+from models.operation.ref import RefOperationModel
 from models.operation.data_collector import OptimizationDataCollector
 from config import config
 
@@ -7,15 +8,12 @@ scenario = OperationScenario(scenario_id=1, config=config)
 
 optimization_model = OptOperationModel(scenario)
 solved_instance = optimization_model.run()
-OptimizationDataCollector(solved_instance, scenario.scenario_id, config).save_yearly_results()
-OptimizationDataCollector(solved_instance, scenario.scenario_id, config).save_hourly_results()
+# OptimizationDataCollector(solved_instance, scenario.scenario_id, config).save_yearly_results()
+# OptimizationDataCollector(solved_instance, scenario.scenario_id, config).save_hourly_results()
 
 
 
-
-
-# scenario = OperationScenario(scenario_id=2, config=config)
-# reference_model = RefOperationModel(scenario)
-# solved_instance = reference_model.run()
+reference_model = RefOperationModel(scenario)
+solved_instance = reference_model.run()
 # ReferenceDataCollector(solved_instance, scenario.scenario_id).save_yearly_results()
 # ReferenceDataCollector(solved_instance, scenario.scenario_id).save_hourly_results()
