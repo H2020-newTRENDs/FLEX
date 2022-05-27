2795a415a58a76176e5f4846c547203beb86dc05
Songmin <yu.ceepcas@gmail.com>
2022-05-27 14:44:17 +0200

    rc model minor refactoring

--- models/operation/rc_model.py
+++ models/operation/rc_model.py
@@ -3,50 +3,47 @@ from models.operation.scenario import OperationScenario
 
 
 class R5C1Model:
+
     def __init__(self, scenario: 'OperationScenario'):
         self.scenario = scenario
-        self.Af = scenario.building.Af
-        self.Hop = scenario.building.Hop
-        self.Htr_w = scenario.building.Htr_w
-        self.Hve = scenario.building.Hve
-        self.CM_factor = scenario.building.CM_factor
-        self.Am_factor = scenario.building.Am_factor
-        self.internal_gains = scenario.building.internal_gains
+        self.building = scenario.building
+        self.init_rc_params()
 
-        self.Atot = 4.5 * self.Af  # 7.2.2.2: Area of all surfaces facing the building zone
-        self.Cm = self.CM_factor * self.Af
-        # (Effective mass related area) [m^2]
-        self.Am = self.Am_factor * self.Af
+    def init_rc_params(self):
+        # TODO: add the norm name and link, so the users can find the equations following their numbers.
+        # 7.2.2.2: Area of all surfaces facing the building zone
+        self.Atot = 4.5 * self.building.Af
+        self.Cm = self.building.CM_factor * self.building.Af
+        # Effective mass related area [m^2]
+        self.Am = self.building.Am_factor * self.building.Af
         # internal gains
-        self.Qi = self.internal_gains * self.Af
-        # Kopplung Temp Luft mit Temp Surface Knoten s
+        self.Qi = self.building.internal_gains * self.building.Af
+        # Coupling Temp Air with Temp Surface Node s
         self.his = np.float_(3.45)  # 7.2.2.2
-        # kopplung zwischen Masse und  zentralen Knoten s (surface)
+        # coupling between mass and central node s (surface)
         self.hms = np.float_(9.1)  # W / m2K from Equ.C.3 (from 12.2.2)
         self.Htr_ms = self.hms * self.Am  # from 12.2.2 Equ. (64)
-        self.Htr_em = 1 / (1 / self.Hop - 1 / self.Htr_ms)  # from 12.2.2 Equ. (63)
-        # thermischer Kopplungswerte W/K
+        self.Htr_em = 1 / (1 / self.building.Hop - 1 / self.Htr_ms)  # from 12.2.2 Equ. (63)
+        # Thermal coupling value W/K
         self.Htr_is = self.his * self.Atot
-        self.Htr_1 = np.float_(1) / (np.float_(1) / self.Hve + np.float_(1) / self.Htr_is)  # Equ. C.6
-        self.Htr_2 = self.Htr_1 + self.Htr_w  # Equ. C.7
+        self.Htr_1 = np.float_(1) / (np.float_(1) / self.building.Hve + np.float_(1) / self.Htr_is)  # Equ. C.6
+        self.Htr_2 = self.Htr_1 + self.building.Htr_w  # Equ. C.7
         self.Htr_3 = 1 / (1 / self.Htr_2 + 1 / self.Htr_ms)  # Equ.C.8
-
         # Equ. C.1
         self.PHI_ia = 0.5 * self.Qi
         self.Q_solar = self.calculate_solar_gains()
 
     def calculate_solar_gains(self) -> np.array:
-        """calculates the solar gains through solar radiation through the effective window area
-        returns the solar gains for all considered building IDs with 8760 rows. Each columns represents a building ID"""
-        AreaWindowEastWest = self.scenario.building.effective_window_area_west_east
-        AreaWindowSouth = self.scenario.building.effective_window_area_south
-        AreaWindowNorth = self.scenario.building.effective_window_area_north
+        """return: 8760h solar gains, calculated with solar radiation and the effective window area."""
+        area_window_east_west = self.building.effective_window_area_west_east
+        area_window_south = self.building.effective_window_area_south
+        area_window_north = self.building.effective_window_area_north
 
-        Q_sol_north = np.outer(np.array(self.scenario.region.radiation_north), AreaWindowNorth)
-        Q_sol_east = np.outer(np.array(self.scenario.region.radiation_east), AreaWindowEastWest / 2)
-        Q_sol_south = np.outer(np.array(self.scenario.region.radiation_south), AreaWindowSouth)
-        Q_sol_west = np.outer(np.array(self.scenario.region.radiation_west), AreaWindowEastWest / 2)
-        Q_solar = ((Q_sol_north + Q_sol_south + Q_sol_east + Q_sol_west).squeeze())
+        Q_solar_north = np.outer(np.array(self.scenario.region.radiation_north), area_window_north)
+        Q_solar_east = np.outer(np.array(self.scenario.region.radiation_east), area_window_east_west / 2)
+        Q_solar_south = np.outer(np.array(self.scenario.region.radiation_south), area_window_south)
+        Q_solar_west = np.outer(np.array(self.scenario.region.radiation_west), area_window_east_west / 2)
+        Q_solar = ((Q_solar_north + Q_solar_south + Q_solar_east + Q_solar_west).squeeze())
         return Q_solar
 
     def calculate_heating_and_cooling_demand(self,
@@ -58,7 +55,7 @@ class R5C1Model:
         building in the beginning of the calculation is achieved. Solar gains are set to 0.
         Returns: heating demand, cooling demand, indoor air temperature, temperature of the thermal mass
         """
-        heating_power_10 = self.Af * 10
+        heating_power_10 = self.building.Af * 10
 
         if self.scenario.space_cooling_technology.power == 0:
             T_air_max = np.full((8760,), 100)  # if no cooling is adopted --> raise max air temperature to 100 so it will never cool:
@@ -94,7 +91,7 @@ class R5C1Model:
             # Equ. C.2
             PHI_m = self.Am / self.Atot * (0.5 * self.Qi + Q_solar[t])
             # Equ. C.3
-            PHI_st = (1 - self.Am / self.Atot - self.Htr_w / 9.1 / self.Atot) * \
+            PHI_st = (1 - self.Am / self.Atot - self.building.Htr_w / 9.1 / self.Atot) * \
                      (0.5 * self.Qi + Q_solar[t])
 
             # (T_sup = T_outside because incoming air is not preheated)
@@ -102,18 +99,18 @@ class R5C1Model:
 
             # Equ. C.5
             PHI_mtot_0 = PHI_m + self.Htr_em * T_outside[t] + self.Htr_3 * (
-                    PHI_st + self.Htr_w * T_outside[t] + self.Htr_1 * (((self.PHI_ia + 0) / self.Hve) + T_sup[t])
+                    PHI_st + self.building.Htr_w * T_outside[t] + self.Htr_1 * (((self.PHI_ia + 0) / self.building.Hve) + T_sup[t])
             ) / self.Htr_2
 
             # Equ. C.5 with 10 W/m^2 heating power
             PHI_mtot_10 = PHI_m + self.Htr_em * T_outside[t] + self.Htr_3 * (
-                    PHI_st + self.Htr_w * T_outside[t] + self.Htr_1 * (
-                    ((self.PHI_ia + heating_power_10) / self.Hve) + T_sup[t])) / self.Htr_2
+                    PHI_st + self.building.Htr_w * T_outside[t] + self.Htr_1 * (
+                    ((self.PHI_ia + heating_power_10) / self.building.Hve) + T_sup[t])) / self.Htr_2
 
             # Equ. C.5 with 10 W/m^2 cooling power
             PHI_mtot_10_c = PHI_m + self.Htr_em * T_outside[t] + self.Htr_3 * (
-                    PHI_st + self.Htr_w * T_outside[t] + self.Htr_1 * (
-                    ((self.PHI_ia - heating_power_10) / self.Hve) + T_sup[t])) / self.Htr_2
+                    PHI_st + self.building.Htr_w * T_outside[t] + self.Htr_1 * (
+                    ((self.PHI_ia - heating_power_10) / self.building.Hve) + T_sup[t])) / self.Htr_2
 
             if t == 0:
                 Tm_t_prev = thermal_start_temperature
@@ -142,30 +139,30 @@ class R5C1Model:
             T_m_10_c = (Tm_t_10_c + Tm_t_prev) / 2
 
             # Euq. C.10
-            T_s_0 = (self.Htr_ms * T_m_0 + PHI_st + self.Htr_w * T_outside[t] + self.Htr_1 *
-                     (T_sup[t] + (self.PHI_ia + 0) / self.Hve)) / (self.Htr_ms + self.Htr_w + self.Htr_1)
+            T_s_0 = (self.Htr_ms * T_m_0 + PHI_st + self.building.Htr_w * T_outside[t] + self.Htr_1 *
+                     (T_sup[t] + (self.PHI_ia + 0) / self.building.Hve)) / (self.Htr_ms + self.building.Htr_w + self.Htr_1)
 
             # Euq. C.10 for 10 W/m^2 heating
-            T_s_10 = (self.Htr_ms * T_m_10 + PHI_st + self.Htr_w * T_outside[t] + self.Htr_1 *
-                      (T_sup[t] + (self.PHI_ia + heating_power_10) / self.Hve)) / (
-                                 self.Htr_ms + self.Htr_w + self.Htr_1)
+            T_s_10 = (self.Htr_ms * T_m_10 + PHI_st + self.building.Htr_w * T_outside[t] + self.Htr_1 *
+                      (T_sup[t] + (self.PHI_ia + heating_power_10) / self.building.Hve)) / (
+                                 self.Htr_ms + self.building.Htr_w + self.Htr_1)
 
             # Euq. C.10 for 10 W/m^2 cooling
-            T_s_10_c = (self.Htr_ms * T_m_10_c + PHI_st + self.Htr_w * T_outside[t] + self.Htr_1 *
-                        (T_sup[t] + (self.PHI_ia - heating_power_10) / self.Hve)) / (
-                                   self.Htr_ms + self.Htr_w + self.Htr_1)
+            T_s_10_c = (self.Htr_ms * T_m_10_c + PHI_st + self.building.Htr_w * T_outside[t] + self.Htr_1 *
+                        (T_sup[t] + (self.PHI_ia - heating_power_10) / self.building.Hve)) / (
+                                   self.Htr_ms + self.building.Htr_w + self.Htr_1)
 
             # Equ. C.11
-            T_air_0 = (self.Htr_is * T_s_0 + self.Hve * T_sup[t] + self.PHI_ia + 0) / \
-                      (self.Htr_is + self.Hve)
+            T_air_0 = (self.Htr_is * T_s_0 + self.building.Hve * T_sup[t] + self.PHI_ia + 0) / \
+                      (self.Htr_is + self.building.Hve)
 
             # Equ. C.11 for 10 W/m^2 heating
-            T_air_10 = (self.Htr_is * T_s_10 + self.Hve * T_sup[t] + self.PHI_ia + heating_power_10) / \
-                       (self.Htr_is + self.Hve)
+            T_air_10 = (self.Htr_is * T_s_10 + self.building.Hve * T_sup[t] + self.PHI_ia + heating_power_10) / \
+                       (self.Htr_is + self.building.Hve)
 
             # Equ. C.11 for 10 W/m^2 cooling
-            T_air_10_c = (self.Htr_is * T_s_10_c + self.Hve * T_sup[t] + self.PHI_ia - heating_power_10) / \
-                         (self.Htr_is + self.Hve)
+            T_air_10_c = (self.Htr_is * T_s_10_c + self.building.Hve * T_sup[t] + self.PHI_ia - heating_power_10) / \
+                         (self.Htr_is + self.building.Hve)
 
 
             # Check if air temperature without heating is in between boundaries and calculate actual HC power:
@@ -181,8 +178,8 @@ class R5C1Model:
             # now calculate the actual temperature of thermal mass Tm_t with Q_HC_real:
             # Equ. C.5 with actual heating power
             PHI_mtot_real = PHI_m + self.Htr_em * T_outside[t] + self.Htr_3 * (
-                    PHI_st + self.Htr_w * T_outside[t] + self.Htr_1 * (
-                    ((self.PHI_ia + heating_demand[t] - cooling_demand[t]) / self.Hve) + T_sup[t])) / self.Htr_2
+                    PHI_st + self.building.Htr_w * T_outside[t] + self.Htr_1 * (
+                    ((self.PHI_ia + heating_demand[t] - cooling_demand[t]) / self.building.Hve) + T_sup[t])) / self.Htr_2
             # Equ. C.4
             Tm_t[t] = (Tm_t_prev * (self.Cm / 3600 - 0.5 * (self.Htr_3 + self.Htr_em)) + PHI_mtot_real) / \
                          (self.Cm / 3600 + 0.5 * (self.Htr_3 + self.Htr_em))
@@ -191,13 +188,13 @@ class R5C1Model:
             T_m_real = (Tm_t[t] + Tm_t_prev) / 2
 
             # Euq. C.10
-            T_s_real = (self.Htr_ms * T_m_real + PHI_st + self.Htr_w * T_outside[t] + self.Htr_1 *
-                        (T_sup[t] + (self.PHI_ia + heating_demand[t] - cooling_demand[t]) / self.Hve)) / \
-                       (self.Htr_ms + self.Htr_w + self.Htr_1)
+            T_s_real = (self.Htr_ms * T_m_real + PHI_st + self.building.Htr_w * T_outside[t] + self.Htr_1 *
+                        (T_sup[t] + (self.PHI_ia + heating_demand[t] - cooling_demand[t]) / self.building.Hve)) / \
+                       (self.Htr_ms + self.building.Htr_w + self.Htr_1)
 
             # Equ. C.11 for 10 W/m^2 heating
-            room_temperature[t] = (self.Htr_is * T_s_real + self.Hve * T_sup[t] + self.PHI_ia +
-                                   heating_demand[t] - cooling_demand[t]) / (self.Htr_is + self.Hve)
+            room_temperature[t] = (self.Htr_is * T_s_real + self.building.Hve * T_sup[t] + self.PHI_ia +
+                                   heating_demand[t] - cooling_demand[t]) / (self.Htr_is + self.building.Hve)
 
         return heating_demand, cooling_demand, room_temperature, Tm_t
 
