4a3436fc67ab864b64eaa6033008d01304605e53
Songmin <yu.ceepcas@gmail.com>
2022-05-27 21:20:57 +0200

    minor formatting

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from config import config
 scenario = OperationScenario(scenario_id=1, config=config)
 
 optimization_model = OptOperationModel(scenario).run()
-OptimizationDataCollector(optimization_model, scenario.scenario_id, config).save_yearly_results()
-OptimizationDataCollector(optimization_model, scenario.scenario_id, config).save_hourly_results()
+# OptimizationDataCollector(optimization_model, scenario.scenario_id, config).save_yearly_results()
+# OptimizationDataCollector(optimization_model, scenario.scenario_id, config).save_hourly_results()
 
 reference_model = RefOperationModel(scenario).run()
-ReferenceDataCollector(reference_model, config).save_yearly_results()
-ReferenceDataCollector(reference_model, config).save_hourly_results()
+# ReferenceDataCollector(reference_model, config).save_yearly_results()
+# ReferenceDataCollector(reference_model, config).save_hourly_results()
