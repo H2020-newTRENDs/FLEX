970d9a854e5e2d7a3514e90497e9177773a682f8
Songmin <yu.ceepcas@gmail.com>
2022-05-30 20:39:29 +0200

    minor formatting

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ from models.operation.scenario import OperationScenario
 from models.operation.opt import OptOperationModel
 from models.operation.ref import RefOperationModel
 from models.operation.data_collector import OptDataCollector, RefDataCollector
+from models.operation.analyzer import Analyzer
 from config import config
 from basics.kit import get_logger
 
 logger = get_logger(__name__)
 
-scenario_ids = [48]
+scenario_ids = [1]
 
 for scenario_id in scenario_ids:
     logger.info(f'Scenario = {scenario_id}')
     scenario = OperationScenario(scenario_id=scenario_id, config=config)
+
     opt_model = OptOperationModel(scenario).run()
     # OptDataCollector(opt_model, scenario.scenario_id, config).run()
-    ref_model = RefOperationModel(scenario).run()
+
+    # ref_model = RefOperationModel(scenario).run()
     # RefDataCollector(ref_model, scenario.scenario_id, config).run()
+
+    # analyzer = Analyzer(scenario, config)
+    # analyzer.run()
+
