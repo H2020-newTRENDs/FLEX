36b5927ca5e949ab462c46730f74e733c9aaf867
Songmin <yu.ceepcas@gmail.com>
2022-05-30 20:53:32 +0200

    minor formatting

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ for scenario_id in scenario_ids:
     logger.info(f'Scenario = {scenario_id}')
     scenario = OperationScenario(scenario_id=scenario_id, config=config)
 
-    opt_model = OptOperationModel(scenario).run()
-    # OptDataCollector(opt_model, scenario.scenario_id, config).run()
+    ref_model = RefOperationModel(scenario).run()
+    RefDataCollector(ref_model, scenario.scenario_id, config).run()
 
-    # ref_model = RefOperationModel(scenario).run()
-    # RefDataCollector(ref_model, scenario.scenario_id, config).run()
+    opt_model = OptOperationModel(scenario).run()
+    OptDataCollector(opt_model, scenario.scenario_id, config).run()
 
     # analyzer = Analyzer(scenario, config)
     # analyzer.run()
