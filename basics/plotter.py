d9dd9c71add01574662bfca05498d9648af488af
Songmin <yu.ceepcas@gmail.com>
2022-06-03 00:25:35 +0200

    operation analyzer updated

--- basics/plotter.py
+++ basics/plotter.py
@@ -14,8 +14,9 @@ class Plotter:
         self.setup_fig_params()
 
     def setup_fig_params(self):
-        self.fig_size = (12, 6)
-        self.fig_axe_area = (0.1, 0.1, 0.8, 0.7)
+        self.fig_size = (12, 7.417)
+        self.fig_axe_area = (0.1, 0.1, 0.8, 0.8)
+        self.fig_axe_area_with_legend = (0.1, 0.1, 0.8, 0.7)
         self.fig_format = "PNG"
         self.fig_dpi = 200
         self.line_width = 1.5
@@ -42,7 +43,7 @@ class Plotter:
         fig.savefig(os.path.join(self.fig_folder, fig_name + ".png"), dpi=self.fig_dpi, format=self.fig_format)
         plt.close(fig)
 
-    def get_figure_plate(self, x_label: str = "X-axis", y_label: str = "Y-axis", x_lim=None, y_lim=None):
+    def get_figure_template(self, x_label: str = "X-axis", y_label: str = "Y-axis", x_lim=None, y_lim=None):
 
         figure = plt.figure(figsize=self.fig_size, dpi=self.fig_dpi, frameon=False)
         ax = figure.add_axes(self.fig_axe_area)
@@ -50,8 +51,11 @@ class Plotter:
         ax.grid(self.grid)
         ax.set_xlabel(x_label, fontsize=self.fontsize, labelpad=self.labelpad)
         ax.set_ylabel(y_label, fontsize=self.fontsize, labelpad=self.labelpad)
-        if x_lim is not None: ax.set_ylim(x_lim)
-        if y_lim is not None: ax.set_ylim(y_lim)
+
+        if x_lim is not None:
+            ax.set_ylim(x_lim)
+        if y_lim is not None:
+            ax.set_ylim(y_lim)
 
         for tick in ax.xaxis.get_major_ticks():
             tick.label1.set_fontsize(self.fontsize)
@@ -61,22 +65,23 @@ class Plotter:
         return figure, ax
 
     def add_legend(self, figure, ax, num_items: int):
+        ax.set_position(self.fig_axe_area_with_legend)
         figure.legend(fontsize=self.legend_fontsize, bbox_to_anchor=self.top_bbox_to_anchor,
                       bbox_transform=ax.transAxes, loc='lower left', ncol=self.legend_ncol(num_items),
                       borderaxespad=0, mode='expand', frameon=True)
 
     def step_figure(self, values_dict: 'Dict[str, Sequence]', fig_name: str,
                     x_label: str = "X-axis", y_label: str = "Y-axis", x_lim=None, y_lim=None):
-        figure, ax = self.get_figure_plate(x_label, y_label, x_lim, y_lim)
-        x = [i + 1 for i in range(0, len(list(values_dict.values())[0]))]
+        figure, ax = self.get_figure_template(x_label, y_label, x_lim, y_lim)
         for key, values in values_dict.items():
+            x = [i + 1 for i in range(0, len(values))]
             ax.step(x, values, where='mid', label=key)
         self.add_legend(figure, ax, len(values_dict))
         self.save_fig(figure, fig_name)
 
     def bar_plot(self, values_dict: 'Dict[str, np.array]', fig_name: str,
                  x_label: str = "X-axis", y_label: str = "Y-axis", x_lim=None, y_lim=None):
-        figure, ax = self.get_figure_plate(x_label, y_label, x_lim, y_lim)
+        figure, ax = self.get_figure_template(x_label, y_label, x_lim, y_lim)
         x = [i + 1 for i in range(0, len(list(values_dict.values())[0]))]
         bottom_positive = np.zeros(len(x), )
         bottom_negative = np.zeros(len(x), )
@@ -91,4 +96,5 @@ class Plotter:
                 bottom_negative += values
         self.add_legend(figure, ax, len(values_dict))
         self.save_fig(figure, fig_name)
+
 
--- operation_ana.py
+++ operation_ana.py
@@ -3,17 +3,52 @@ from config import config
 
 
 class ProjectOperationAnalyzer(OperationAnalyzer):
-    pass
+
+    def plot_scenario_comparison(self):
+        self.compare_opt_ref(1)
+        self.compare_opt(id1=1, id2=16)
+        self.compare_ref(id1=1, id2=16)
+
+    def plot_scenario_electricity_balance(self):
+        scenario_ids = [1, 96]
+        hour_ranges = [(25, 192), (5761, 5928)]
+        models = ["opt", "ref"]
+        for scenario_id in scenario_ids:
+            for model in models:
+                for hour_range in hour_ranges:
+                    self.plot_electricity_balance(
+                        scenario_id=scenario_id,
+                        model=model,
+                        start_hour=hour_range[0],
+                        end_hour=hour_range[1]
+                    )
+
+    def summarize_operation_energy_cost(self):
+        self.create_operation_energy_cost_table()
+        self.plot_operation_energy_cost_curve()
+
+    def summarize_operation_energy_cost_change(self):
+        component_changes = [
+            ("ID_Building", 3, 2),
+            ("ID_Building", 2, 1),
+            ("ID_Building", 3, 1),
+            ("ID_SEMS", 2, 1),
+            ("ID_Boiler", 2, 1),
+            ("ID_SpaceHeatingTank", 2, 1),
+            ("ID_HotWaterTank", 2, 1),
+            ("ID_PV", 2, 1),
+            ("ID_Battery", 2, 1)
+        ]
+        # self.create_component_energy_cost_change_tables(component_changes)
+        # self.plot_operation_energy_cost_change_curve(component_changes)
+        self.get_building_pathway(191, component_changes)
 
 
 if __name__ == "__main__":
     ana = ProjectOperationAnalyzer(config)
-    # ana.compare_opt_ref(1)
-    # ana.compare_opt(id1=1, id2=16)
-    # ana.compare_ref(id1=1, id2=16)
-    ana.plot_operation_energy_cost_curve()
-    # ana.plot_electricity_balance(scenario_id=1, model="opt", start_hour=25, end_hour=192)
-    # ana.plot_electricity_balance(scenario_id=1, model="ref", start_hour=25, end_hour=192)
-    # ana.plot_electricity_balance(scenario_id=1, model="opt", start_hour=5761, end_hour=5928)
-    # ana.plot_electricity_balance(scenario_id=1, model="ref", start_hour=5761, end_hour=5928)
+    ana.summarize_operation_energy_cost_change()
+
+
+
+
 
