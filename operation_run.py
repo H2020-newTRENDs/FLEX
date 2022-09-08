715064453c51830ebd2769ca7ea428c2dcd02b7d
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2022-09-08 20:59:53 +0800

    db and scenario table setup

--- operation_run.py
+++ operation_run.py
@@ -4,8 +4,6 @@ from basics.kit import get_logger
 from config import config
 from models.operation.data_collector import OptDataCollector
 from models.operation.data_collector import RefDataCollector
-from models.operation.model_opt import FuelBoilerOptInstance
-from models.operation.model_opt import FuelBoilerOptOperationModel
 from models.operation.model_opt import OptInstance
 from models.operation.model_opt import OptOperationModel
 from models.operation.model_ref import RefOperationModel
@@ -15,15 +13,12 @@ logger = get_logger(__name__)
 
 if __name__ == "__main__":
     hp_instance = OptInstance().create_instance()
-
-    scenario_ids = np.arange(1, 2049)
+    scenario_ids = np.arange(1, 641)
     for scenario_id in scenario_ids:
         logger.info(f"FlexOperation --> Scenario = {scenario_id}.")
         scenario = OperationScenario(scenario_id=scenario_id, config=config)
         ref_model = RefOperationModel(scenario).solve()
-
         opt_model = OptOperationModel(scenario).solve(hp_instance)
-
         RefDataCollector(ref_model, scenario.scenario_id, config, save_hour_results=False).run()
         OptDataCollector(opt_model, scenario.scenario_id, config, save_hour_results=False).run()
 
