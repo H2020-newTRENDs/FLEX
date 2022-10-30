2f048e3d82e4581bb03a25d38b50b2fa71699db9
PhilippMaschi <phil.masch@hotmail.com>
2022-10-30 12:40:29 +0100

    input data back to original for the validation

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import logging
 from basics.config import Config
 
 logging.getLogger("pyomo.core").setLevel(logging.ERROR)
-config = Config(project_name="Flex_20")
+config = Config(project_name="Flex_5R1C_validation")
 
 
 def get_config(project_name: str):
