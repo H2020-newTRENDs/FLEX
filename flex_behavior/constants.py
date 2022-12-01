320f3f218d74c810203233939ff58d1ee4aeef43
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2022-12-01 14:12:53 +0800

    FLEX-Behavior model framework created

--- flex_behavior/constants.py
+++ flex_behavior/constants.py
@@ -4,5 +4,5 @@ from dataclasses import dataclass
 @dataclass
 class BehaviorTable:
     Scenarios = "BehaviorScenario"
-    ID_Activity = "BehaviorScenario_ID_Activity"
+    ID_Activity = "BehaviorScenario_Activity"
     ActivityProfile = "BehaviorScenario_ActivityProfile"
