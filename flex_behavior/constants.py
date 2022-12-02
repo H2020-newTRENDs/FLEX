dfb5ae54ca4223a269a54cb3b5fd92f3ce54a666
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2022-12-02 11:54:20 +0800

    new behavior tables added

--- flex_behavior/constants.py
+++ flex_behavior/constants.py
@@ -4,5 +4,6 @@ from dataclasses import dataclass
 @dataclass
 class BehaviorTable:
     Scenarios = "BehaviorScenario"
-    ID_Activity = "BehaviorScenario_Activity"
+    ID_Activity = "BehaviorScenario_ActivityInfo"
+    ID_Technology = "BehaviorScenario_TechnologyInfo"
     ActivityProfile = "BehaviorScenario_ActivityProfile"
