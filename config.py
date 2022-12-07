2481c921d820c8219ec314dd3fbc4f1b8dfaac47
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2022-12-08 07:52:29 +0800

    ignore updated

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import logging
 from flex.config import Config
 
 logging.getLogger("pyomo.core").setLevel(logging.ERROR)
-cfg = Config(project_name="FLEX_BuildingTest")
+cfg = Config(project_name="FLEX")
