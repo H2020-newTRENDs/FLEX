f06035fa8bac6aabb934e3a03785a4bb4c0abd42
Wollmonster <rickmann99@web.de>
2023-02-21 23:39:11 +0100

    Added Type of Day in probability matrices

--- flex_behavior/activity-prediction/markov_model.py
+++ flex_behavior/activity-prediction/markov_model.py
@@ -25,19 +25,19 @@ class MarkovModel:
 
         return changes
 
-    def compute_change_probability(self, changes) -> "Dict[Tuple[int, int], double]":
+    def compute_change_probability(self, changes, tod) -> "Dict[Tuple[str, int, int], double]":
         probabilities = {}
         for before in range(1, self.n_activities + 1):
             n_changes_from_before = sum(v for k, v in changes.items() if k[0] == before)
             if n_changes_from_before == 0:
-                probabilities[(before, 1)] = 0
+                probabilities[(tod, before, 1)] = 0
             else:
-                probabilities[(before, 1)] = changes[(before, 1)] / n_changes_from_before
+                probabilities[(tod, before, 1)] = changes[(before, 1)] / n_changes_from_before
             for now in range(2, self.n_activities + 1):
                 if n_changes_from_before == 0:
-                    probabilities[(before, now)] = 0
+                    probabilities[(tod, before, now)] = 0
                 else:
-                    probabilities[(before, now)] = probabilities[(before, now - 1)] + changes[
+                    probabilities[(tod, before, now)] = probabilities[(tod, before, now - 1)] + changes[
                         (before, now)] / n_changes_from_before
         return probabilities
 
@@ -49,9 +49,9 @@ class MarkovModel:
 
     def save_probabilities(self, prob_dict, filename: str, column_name):
         data = []
-        for t in prob_dict:
-            for key, value in prob_dict[t].items():
-                data.append([t, key[0], key[1], value])
+        for (tod, t) in prob_dict:
+            for key, value in prob_dict[(tod, t)].items():
+                data.append([tod, t, key[1], key[2], value])
         df = pd.DataFrame(data, columns=column_name)
         filepath = os.path.join(self.datadir, 'output', filename)
         df.to_excel(filepath, index=False)
@@ -64,55 +64,73 @@ class MarkovModel:
     def build_change_matrix(self):
         filepath = os.path.join(self.datadir, 'input_behavior', 'BehaviorScenario_ActivityProfile.xlsx')
         df = pd.read_excel(filepath)
-        # Filter ToD and Person Classification before or use already filtered data
 
         prob_dict = {}
-        for t in range(2, 145):
-            before, now = self.get_activity_list(df, t)
-            changes = model.compute_activity_changes(before, now)
-            probability = model.compute_change_probability(changes)
-            prob_dict[t] = probability
+        for tod in ['week', 'friday', 'saturday', 'sunday']:
+            df_tod = self.filter_tod(df, tod)
 
-        column_name = ['t', 'activity at t-1', 'activity at t', 'probability']
+            for t in range(2, 145):
+                before, now = self.get_activity_list(df_tod, t)
+                changes = model.compute_activity_changes(before, now)
+                probability = model.compute_change_probability(changes, tod)
+                prob_dict[(tod, t)] = probability
+
+        column_name = ['ToD', 't', 'activity at t-1', 'activity at t', 'probability']
         model.save_probabilities(prob_dict, 'change-probability.xlsx', column_name)
 
-    def build_duration_matrix(self):
+    def build_duration_matrix(self, filename: str):
         filepath = os.path.join(self.datadir, 'input_behavior', 'BehaviorScenario_ActivityProfile.xlsx')
         df = pd.read_excel(filepath)
         # Filter ToD and Person Classification before or use already filtered data
-        df = df.drop(columns=['id_hhx', 'id_persx', 'id_tagx', 'monat', 'wtagfei'])
-        values = df.values.tolist()
-
-        durations = []
-        for row in values:
-            act_duration = self.count_occurences_in_list(row)
-            durations.extend(act_duration)
+        df = df.drop(columns=['id_hhx', 'id_persx', 'id_tagx', 'monat'])
 
         prob_dict = {}
-        for t in range(1,145):
-            time_filtered = filter(lambda x: x[0] == t, durations)
-            prob_dict[t] = self.compute_duration_probability(list(time_filtered))
 
-        column_name = ['t', 'activity', 'duration', 'probability']
+        for tod in ['week', 'friday', 'saturday', 'sunday']:
+            df_tod = self.filter_tod(df, tod)
+            values = df_tod.drop(columns=['wtagfei']).values.tolist()
+
+            durations = []
+            for row in values:
+                act_duration = self.count_occurences_in_list(row)
+                durations.extend(act_duration)
+
+            for t in range(1, 145):
+                time_filtered = filter(lambda x: x[0] == t, durations)
+                prob_dict[(tod, t)] = self.compute_duration_probability(list(time_filtered), tod)
+
+        column_name = ['ToD', 't', 'activity', 'duration', 'probability']
         model.save_probabilities(prob_dict, 'duration-probability.xlsx', column_name)
+
+    def filter_tod(self, df, tod):
+        type_of_days = {'week': range(1, 5),
+                        'friday': [5],
+                        'saturday': [6],
+                        'sunday': [7]}
+        df_tod = df[df['wtagfei'].isin(type_of_days[tod])]
+        return df_tod
 
     def compute_duration_probability(
             self,
             act_duration: "List[Tuple[int, int, int]]",  # (timestep, activity, duration)
-    ) -> "Dict[Tuple[int, int], double]":  # (activity, duration): accumulated probability
+            tod: int,  # type of day
+    ) -> "Dict[Tuple[int, int, int], double]":  # (activity, duration): accumulated probability
         probabilities = {}  # keys = (activity, duration)
         for act in range(1, self.n_activities + 1):
             act_list = [x[2] for x in act_duration if x[1] == act]
             n_durations = len(act_list)
             if n_durations == 0:
-                probabilities[(act, 1)] = 0
+                probabilities[(tod, act, 1)] = 0
             else:
-                probabilities[(act, 1)] = act_list.count(1) / n_durations
+                probabilities[(tod, act, 1)] = act_list.count(1) / n_durations
             for duration in range(2, 145):
                 if n_durations == 0:
-                    probabilities[(act, duration)] = 0
+                    probabilities[(tod, act, duration)] = 0
                 else:
-                    probabilities[(act, duration)] = probabilities[(act, duration-1)] + act_list.count(duration) / n_durations
+                    probabilities[(tod, act, duration)] = probabilities[(tod, act, duration - 1)] + act_list.count(
+                        duration) / n_durations
+                    if probabilities[(tod, act, duration)] == 1.0:
+                        break
         return probabilities
 
     def count_occurences_in_list(
