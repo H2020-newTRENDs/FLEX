3852e72ab0a0515282c1c91c0c1fbc6e4ef3b2f9
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2023-02-27 23:02:04 +0100

    developing Behavior model

--- flex_behavior/household.py
+++ flex_behavior/household.py
@@ -8,21 +8,52 @@ if TYPE_CHECKING:
 class Person:
     def __init__(self, scenario: "BehaviorScenario", id_person_type):
         self.scenario = scenario
+        self.timeslot_num = 144  # each timeslot represents 10min, in total 144 timeslots in a day
         self.id_person_type = id_person_type
 
     def setup(self):
+        self.setup_activity_profile()
         self.setup_building_occupancy_profile()
         self.setup_electricity_demand_profile()
         self.setup_hot_water_demand_profile()
         self.setup_driving_profile()
 
     def setup_activity_profile(self):
-        self.activity_profile = np.zeros(self.scenario.period_num)
+        self.activity_profile = []
+        for day in range(1, 366):
+            id_day_type = self.scenario.day_type[day % 7]
+            sleep_duration = self.scenario.get_activity_duration(self.id_person_type, id_day_type, 1, 1)
+            day_activities = [1] * sleep_duration  # sleeping, at the beginning of the day (4:00 am)
+            while len(day_activities) <= self.timeslot_num:
+                timeslot = len(day_activities) + 1
+                id_activity_before = day_activities[-1]
+                id_activity_now = self.scenario.get_activity_now(
+                    self.id_person_type,
+                    id_day_type,
+                    id_activity_before,
+                    timeslot
+                )
+                duration = self.scenario.get_activity_duration(
+                    self.id_person_type,
+                    id_day_type,
+                    id_activity_now,
+                    timeslot
+                )
+                day_activities.extend([id_activity_now] * duration)
+            day_activities = day_activities[0: 144]
+            day_activities = day_activities[-24:] + day_activities[:-24]
+            self.activity_profile.extend(day_activities)
 
     def setup_building_occupancy_profile(self):
         self.building_occupancy = np.ones(self.scenario.period_num)
 
     def setup_electricity_demand_profile(self):
+        ...
+
+    def setup_base_electricity_demand_profile(self):
+        ...
+
+    def setup_activity_electricity_demand_profile(self):
         self.electricity_demand = np.zeros(self.scenario.period_num)
         for id_activity in self.activity_profile:
             id_technology = self.scenario.get_activity_technology(id_activity)
--- flex_behavior/scenario.py
+++ flex_behavior/scenario.py
@@ -15,11 +15,14 @@ class BehaviorScenario:
         self.db = create_db_conn(self.config)
         self.plotter = Plotter(config)
         self.period_num = 8760
+        # imported from excel input: BehaviorScenario
         self.id_household_type: Optional[int] = None
+        self.setup()
 
     def setup(self):
         self.setup_scenario_params()
         self.import_scenario_data()
+        self.setup_day_type()
 
     def setup_scenario_params(self):
         df = self.db.read_dataframe(BehaviorTable.Scenarios)
@@ -31,10 +34,49 @@ class BehaviorScenario:
 
     def import_scenario_data(self):
         self.household_composition = self.db.read_dataframe(BehaviorTable.HouseholdComposition)
-        self.activity_profile = self.db.read_dataframe(BehaviorTable.ActivityProfile)
+        self.activity_change_prob = self.db.read_dataframe(BehaviorTable.ActivityChangeProb)
+        self.activity_duration_prob = self.db.read_dataframe(BehaviorTable.ActivityDurationProb)
         self.technology_trigger_prob = self.db.read_dataframe(BehaviorTable.TechnologyTriggerProbability)
         self.technology_power_active = self.db.read_dataframe(BehaviorTable.TechnologyPowerActive)
         self.technology_power_standby = self.db.read_dataframe(BehaviorTable.TechnologyPowerStandby)
+
+    def setup_day_type(self):
+        self.day_type = {
+            1: 1,  # Monday
+            2: 1,  # Tuesday
+            3: 1,  # Wednesday
+            4: 1,  # Thursday
+            5: 2,  # Friday
+            6: 3,  # Saturday
+            0: 4,  # Sunday --> weekday % 7 = 0
+        }
+
+    @staticmethod
+    def filter_dataframe_smart(df, columns, values):
+        while len(df[df[columns] == values]) == 0 and len(columns) > 1:
+            columns = columns[:-1]
+            values = values[:-1]
+        filtered_df = df[df[columns] == values]
+        print()
+        return filtered_df
+
+    def get_activity_duration(self, id_person_type: int, id_day_type: int, id_activity: int, timeslot: int):
+        columns = ["ID_Activity", "t", "ID_PersonType", "ID_DayType"]
+        values = [id_activity, timeslot, id_person_type, id_day_type]
+        df = self.filter_dataframe_smart(self.activity_duration_prob.copy(), columns, values)
+        d = {}
+        for index, row in df.iterrows():
+            d[row["duration"]] = row["probability"]
+        return int(kit.dict_sample(d))
+
+    def get_activity_now(self, id_person_type: int, id_day_type: int, id_activity_before: int, timeslot: int):
+        columns = ["ID_ActivityBefore", "t", "ID_PersonType", "ID_DayType"]
+        values = [id_activity_before, timeslot, id_person_type, id_day_type]
+        df = self.filter_dataframe_smart(self.activity_change_prob.copy(), columns, values)
+        d = {}
+        for index, row in df.iterrows():
+            d[row["ID_ActivityNow"]] = row["probability"]
+        return int(kit.dict_sample(d))
 
     def get_household_composition(self, id_household_type: int):
         df = self.household_composition.loc[self.household_composition["ID_HouseholdType"] == id_household_type]
