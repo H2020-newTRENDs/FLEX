bf47e2631e1e086b5e3abc782029006d54070990
Wollmonster <rickmann99@web.de>
2023-03-16 20:08:01 +0100

    generate person activity profile continuously

--- flex_behavior/person.py
+++ flex_behavior/person.py
@@ -21,30 +21,36 @@ class Person:
         self.setup_electricity_and_hotwater_demand_profile()
 
     def setup_activity_profile(self):
-        for day in range(2, 367):  # The year of 2019 started with Tuesday
-            print(f'day = {day}')
+        day = 2  # The year of 2019 started with Tuesday
+        id_day_type = self.scenario.day_type[day % 7]
+        init_timeslot = 121  # 0:00 am
+        sleep_duration = self.scenario.get_activity_duration(self.id_person_type, id_day_type, 1, init_timeslot)
+        activities = [1] * sleep_duration  # sleeping, at the beginning of the day (0:00 am)
+        day = (len(activities) // self.timeslot_num) + 2  # + 1 to get days from 1 to 365, +1 bc yaer starts at day 2
+        prev_day = day
+        while day < 367:
+            if day != prev_day:
+                print('day:', day)
+                prev_day = day
             id_day_type = self.scenario.day_type[day % 7]
-            sleep_duration = self.scenario.get_activity_duration(self.id_person_type, id_day_type, 1, 120)
-            day_activities = [1] * sleep_duration  # sleeping, at the beginning of the day (4:00 am)
-            while len(day_activities) <= self.timeslot_num:
-                timeslot = len(day_activities) + 1
-                id_activity_before = day_activities[-1]
-                id_activity_now = self.scenario.get_activity_now(
-                    self.id_person_type,
-                    id_day_type,
-                    id_activity_before,
-                    timeslot
-                )
-                duration = self.scenario.get_activity_duration(
-                    self.id_person_type,
-                    id_day_type,
-                    id_activity_now,
-                    timeslot
-                )
-                day_activities.extend([id_activity_now] * duration)
-            day_activities = day_activities[0: 144]
-            day_activities = day_activities[-24:] + day_activities[:-24]
-            self.activity_profile.extend(day_activities)
+            timeslot = (init_timeslot + len(activities)) % self.timeslot_num + 1
+            id_activity_before = activities[-1]
+            id_activity_now = self.scenario.get_activity_now(
+                self.id_person_type,
+                id_day_type,
+                id_activity_before,
+                timeslot
+            )
+            duration = self.scenario.get_activity_duration(
+                self.id_person_type,
+                id_day_type,
+                id_activity_now,
+                timeslot
+            )
+            activities.extend([id_activity_now] * duration)
+            day = (len(activities) // self.timeslot_num) + 2 # + 1 to get days from 1 to 365, +1 bc yaer starts at day 2
+        activities = activities[:(365 * self.timeslot_num)]
+        self.activity_profile = activities
 
     def setup_electricity_and_hotwater_demand_profile(self):
         self.electricity_demand = [0] * len(self.activity_profile)
