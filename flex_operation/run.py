3207b84250e26599e34da7ee1edff7ef5b87efa3
KevanUU <k.m.skorna@students.uu.nl>
2023-04-22 20:26:38 +0200

    Added a mean energy demand plot.

--- flex_operation/run.py
+++ flex_operation/run.py
@@ -56,5 +56,7 @@ def run_operation_analyzer(
 
 def run_operation_analyzer_kevan(config: "Config", scenario_id: int):
     ana = OperationAnalyzer(config)
-    ana.plot_scenario_electricity_balance(scenario_id=scenario_id)
+    # ana.plot_scenario_electricity_balance(scenario_id=scenario_id)
     # ana.plot_electricity_balance_demand(scenario_id=scenario_id, model="ref")
+    # ana.plot_scenario_energy_demand(scenario_id=scenario_id)
+    ana.plot_scenario_energy_demand_mean(scenario_id=scenario_id)
