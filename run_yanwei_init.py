8aed10df8f29b4021983dcdb60685a3a97210c29
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2023-07-02 23:20:39 +0200

    EU27 finished

--- run_yanwei_init.py
+++ run_yanwei_init.py
@@ -7,15 +7,31 @@ from db_init import ProjectDatabaseInit
 from flex.config import Config
 
 
-def gen_new_scenario_table():
-    folder_path = 'data/output'  # Specify the path to the folder you want to explore
-    folder_names = [name for name in os.listdir(folder_path) if
-                    os.path.isdir(os.path.join(folder_path, name)) and
-                    not name.startswith("EV") and
-                    not name.startswith("no")]
+def get_eu27_names():
+    folder_path = 'data/output'
+    return [
+        name for name in os.listdir(folder_path) if
+        os.path.isdir(os.path.join(folder_path, name)) and
+        not name.startswith("EV") and
+        not name.startswith("no")
+    ]
 
-    for index, region in enumerate(folder_names):
-        print(f'{region} --> {index + 1}/{len(folder_names)}')
+
+def check_temperature():
+    folder_path = 'data/output'
+    eu27_names = get_eu27_names()
+    for index, region in enumerate(eu27_names):
+        conn = sqlalchemy.create_engine(f'sqlite:///{os.path.join(folder_path, region, region + ".sqlite")}')
+        df = pd.read_sql('select * from ' + "OperationScenario_RegionWeather", con=conn)
+        print(f'{region} --> {index + 1}/{len(eu27_names)}: [{min(df["temperature"])}, {max(df["temperature"])}]')
+
+
+def gen_new_scenario_table():
+    folder_path = 'data/output'
+    eu27_names = get_eu27_names()
+
+    for index, region in enumerate(eu27_names):
+        print(f'{region} --> {index + 1}/{len(eu27_names)}')
         conn = sqlalchemy.create_engine(f'sqlite:///{os.path.join(folder_path, region, region + ".sqlite")}')
         df = pd.read_sql('select * from ' + "OperationScenario", con=conn)
         df1 = df.loc[(df["ID_SpaceHeatingTank"] == 1) & (df["ID_HotWaterTank"] == 1)]
@@ -38,7 +54,6 @@ def init_eu27():
         # 'LV', 'IT', 'CZ', 'RO', 'PT', 'SI', 'HR',
         # 'HU', 'NL', 'BG', 'AT', 'DE', 'DK', 'FI',
         # 'LU', 'FR', 'ES', 'IE', 'LT', 'CY'
-        'LT'
     ]
     for index, region in enumerate(regions):
         print(f'{region} --> {index + 1}/{len(regions)}')
@@ -46,8 +61,9 @@ def init_eu27():
 
 
 if __name__ == "__main__":
-    init_eu27()
+    # init_eu27()
     # gen_new_scenario_table()
+    check_temperature()
 
 
 
