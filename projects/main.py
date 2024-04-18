3a6c3d7ccd5ae5a8e685dafb849dc06d7f959c56
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-04-18 09:34:11 +0800

    minor

--- projects/main.py
+++ projects/main.py
@@ -1,12 +1,13 @@
 import os
 import random
-from projects.analysis.main_operation import run_operation_model
-from projects.analysis.main_operation import run_operation_model_parallel
+from typing import List
+
 from plotters.operation import household_load_balance
-from utils.config import Config
-from utils.db import init_project_db
 from projects.analysis import generate_summary
 from projects.analysis import process_summary
+from projects.analysis.main_operation import run_operation_model_parallel
+from utils.config import Config
+from utils.db import init_project_db
 
 
 def get_config(project_name: str):
@@ -18,7 +19,6 @@ def get_config(project_name: str):
 
 def run_flex_operation_model(config: "Config"):
     init_project_db(config)
-    # run_operation_model(config=config, save_hour=True, scenario_ids=[1])
     run_operation_model_parallel(config=config, task_num=8, save_hour=True)
 
 
@@ -26,21 +26,32 @@ def run_flex_operation_plotter(config: "Config"):
     household_load_balance(config, scenario_ids=random.sample(range(1, 4375), 100))
 
 
-if __name__ == "__main__":
+def run_scenario_and_generate_summary_files(scenarios: List[str]):
+    for scenario in scenarios:
+        cfg = get_config(scenario)
+        run_flex_operation_model(cfg)
+        generate_summary.gen_summary_year(cfg)
+        generate_summary.gen_summary_hour(cfg)
 
-    cfg = get_config("DEU_2030")
 
-    # run model and plotter
-    # run_flex_operation_model(cfg)
-    # run_flex_operation_plotter(cfg)
-
-    # generate summary
-    # generate_summary.gen_summary_year(cfg)
-    # generate_summary.gen_summary_hour(cfg)
-
-    # process summary
+def process_summary_files():
     process_summary.concat_summary()
     process_summary.process_summary_year()
-    # process_summary.process_summary_hour()
+    process_summary.process_summary_hour()
     process_summary.compare_hwb_diff()
+
+
+if __name__ == "__main__":
+
+    SCENARIOS = [
+        # "CZE_2040",
+        "FIN_2040",
+        "GRC_2040",
+        "LTU_2040",
+        "LVA_2040",
+        "ROU_2040",
+        "SWE_2040",
+    ]
+    run_scenario_and_generate_summary_files(SCENARIOS)
+
 
