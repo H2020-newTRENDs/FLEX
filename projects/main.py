38a493277c231640e3966352b5b7cac8c90e2533
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-04-18 09:39:42 +0800

    minor

--- projects/main.py
+++ projects/main.py
@@ -1,5 +1,6 @@
 import os
 import random
+from typing import List
 from projects.analysis.main_operation import run_operation_model
 from models.operation.main import run_operation_model_parallel
 from plotters.operation import household_load_balance
@@ -26,21 +27,31 @@ def run_flex_operation_plotter(config: "Config"):
     household_load_balance(config, scenario_ids=random.sample(range(1, 4375), 100))
 
 
-if __name__ == "__main__":
+def run_scenario_and_generate_summary_files(scenarios: List[str]):
+    for scenario in scenarios:
+        cfg = get_config(scenario)
+        run_flex_operation_model(cfg)
+        generate_summary.gen_summary_year(cfg)
+        generate_summary.gen_summary_hour(cfg)
 
-    cfg = get_config("DEU_2030")
 
-    # run model and plotter
-    # run_flex_operation_model(cfg)
-    # run_flex_operation_plotter(cfg)
-
-    # generate summary
-    # generate_summary.gen_summary_year(cfg)
-    # generate_summary.gen_summary_hour(cfg)
-
-    # process summary
+def process_summary_files():
     process_summary.concat_summary()
     process_summary.process_summary_year()
-    # process_summary.process_summary_hour()
+    process_summary.process_summary_hour()
     process_summary.compare_hwb_diff()
+
+
+if __name__ == "__main__":
+
+    SCENARIOS = [
+        "FIN_2040",
+        "GRC_2040",
+        "LTU_2040",
+        "LVA_2040",
+        "ROU_2040",
+        "SWE_2040",
+    ]
+    run_scenario_and_generate_summary_files(SCENARIOS)
+
 
