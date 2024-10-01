5a0cf7bbfed7246dd878ba3029d10911b3407095
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-10-01 18:41:22 +0800

    test project added

--- projects/main.py
+++ projects/main.py
@@ -15,7 +15,7 @@ def get_config(project_name: str):
 
 def run_flex_operation_model(config: "Config"):
     init_project_db(config)
-    run_operation_model(config=config, save_hour=True, scenario_ids=[1])
+    run_operation_model(config=config, save_hour=True)
 
 
 def run_flex_operation_plotter(config: "Config"):
@@ -24,6 +24,6 @@ def run_flex_operation_plotter(config: "Config"):
 
 if __name__ == "__main__":
 
-    cfg = get_config("PROJECT_FOLDER_NAME")
+    cfg = get_config("TEST")
     run_flex_operation_model(cfg)
-    run_flex_operation_plotter(cfg)
+    # run_flex_operation_plotter(cfg)
